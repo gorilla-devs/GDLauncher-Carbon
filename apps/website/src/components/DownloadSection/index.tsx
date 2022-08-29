9b2c0210dd1aaacf40b20fe0279932ea60143dda
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2022-08-29 21:31:56 +0200

    style fix

--- apps/website/src/components/DownloadSection/index.tsx
+++ apps/website/src/components/DownloadSection/index.tsx
@@ -12,7 +12,7 @@ const DownloadSection: Component<{ pathname: string }> = ({ pathname }) => {
       <div class="absolute top-0 bottom-0 left-0 right-0">
         <div class="absolute top-0 bottom-0 left-0 right-0 z-10 opacity-95 bg-slate-800"></div>
       </div>
-      <div class="w-4/5 lg:w-full z-20 flex flex-col-reverse lg:flex-row justify-between items-center gap-20 max-w-7xl">
+      <div class="w-4/5 lg:w-full z-20 flex flex-col-reverse lg:flex-row justify-between items-center gap-10 lg:gap-20 max-w-7xl">
         <div class="py-0 px-4 max-w-2xl">
           <h3 class="text-5xl lg:text-8xl mb-2 font-bold">
             {t("download.title")}
