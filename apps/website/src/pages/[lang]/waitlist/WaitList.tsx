f91826dc73cae6c86bce2dea6938ae681951d1aa
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2022-09-02 18:25:16 +0200

    waitlist font size

--- apps/website/src/pages/[lang]/waitlist/WaitList.tsx
+++ apps/website/src/pages/[lang]/waitlist/WaitList.tsx
@@ -6,15 +6,15 @@ const WaitList = ({ pathname }: { pathname: string }) => {
   const t = useTranslations(pathname);
   return (
     <div class="h-[calc(100vh-5.5rem)] lg:h-screen relative flex flex-col justify-center items-center">
-      <div class="flex flex-col justify-center items-center gap-10 max-w-5xl text-center">
-        <h1 class="flex flex-col lg:block text-2xl lg:text-7xl font-bold ">
+      <div class="flex flex-col justify-center items-center gap-10 max-w-xs lg:max-w-5xl text-center">
+        <h1 class="flex flex-col lg:block text-4xl lg:text-7xl font-bold ">
           {t("waitlist.title")}
           <span class="inline-block px-4 text-yellow-500 -rotate-10 hover:rotate-0 transition-transform underline">
             {t("waitlist.title2")}
           </span>
           {t("waitlist.title3")}
         </h1>
-        <p class="text-xl lg:text-3xl font-thin max-w-4xl lg">
+        <p class="text-xl lg:text-3xl font-thin max-w-xs lg:max-w-4xl">
         {t("waitlist.text")}
         </p>
         <div class="flex flex-col justify-center items-center gap-10">
@@ -25,8 +25,8 @@ const WaitList = ({ pathname }: { pathname: string }) => {
           <div class="w-full">
             <Input placeholder={t("waitlist.kofi_email")} type="email" />
           </div>
+          <Button class="min-w-[260px] border-none transition duration-150 box-shadow-button hover:box-shadow-button-hover active:box-shadow-button-active">{t("waitlist.getAccess")}</Button>
         </div>
-          <Button class="border-none transition duration-150 box-shadow-button hover:box-shadow-button-hover active:box-shadow-button-active">{t("waitlist.getAccess")}</Button>
       </div>
     </div>
   );
