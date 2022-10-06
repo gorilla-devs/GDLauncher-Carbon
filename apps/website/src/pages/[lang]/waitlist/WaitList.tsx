c7e85b6c0fe3ea029a142feecc737ba7d8e45781
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2022-10-06 09:13:11 +0200

    responsive fix

--- apps/website/src/pages/[lang]/waitlist/WaitList.tsx
+++ apps/website/src/pages/[lang]/waitlist/WaitList.tsx
@@ -57,7 +57,7 @@ const WaitList = ({ pathname }: { pathname: string }) => {
   };
 
   return (
-    <div class="h-[calc(100vh-5.5rem)] lg:h-screen relative flex flex-col justify-center items-center">
+    <div class="pt-10 lg:h-screen relative flex flex-col justify-center items-center">
       <div class="flex flex-col justify-center items-center gap-10 max-w-xs lg:max-w-5xl text-center lg:pt-24">
         <h1 class="flex flex-col lg:block text-4xl lg:text-7xl font-bold ">
           {t("waitlist.title")}
