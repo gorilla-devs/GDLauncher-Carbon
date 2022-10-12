b51621f44536f87594290ad73f59040ab9717b5f
Davide <davide.ceschia@gmail.com>
2022-10-12 02:13:15 +0200

    e2e tests

---
+++ apps/desktop/__tests__/example.spec.ts
@@ -0,0 +1,50 @@
+import { expect, test } from "@playwright/test";
+import {
+  clickMenuItemById,
+  ipcMainCallFirstListener,
+  ipcRendererCallFirstListener,
+  ipcMainInvokeHandler,
+  ipcRendererInvoke,
+} from "electron-playwright-helpers";
+import path from "path";
+import { ElectronApplication, Page, _electron as electron } from "playwright";
+
+let electronApp: ElectronApplication;
+
+test.beforeAll(async () => {
+  // set the CI environment variable to true
+  process.env.CI = "e2e";
+  electronApp = await electron.launch({
+    args: [],
+    executablePath: path.join(
+      __dirname,
+      "../release/1.5.5/mac-arm64/GDLauncher Carbon.app/Contents/MacOS/GDLauncher Carbon"
+    ),
+  });
+  electronApp.on("window", async (page) => {
+    const filename = page.url()?.split("/").pop();
+    console.log(`Window opened: ${filename}`);
+
+    // capture errors
+    page.on("pageerror", (error) => {
+      console.error(error);
+    });
+    // capture console messages
+    page.on("console", (msg) => {
+      console.log(msg.text());
+    });
+  });
+});
+
+test.afterAll(async () => {
+  await electronApp.close();
+});
+
+let page: Page;
+
+test("renders the first page", async () => {
+  page = await electronApp.firstWindow();
+  await page.waitForSelector("#root");
+  const title = await page.title();
+  expect(title).toBe("GDLauncher Carbon");
+});
