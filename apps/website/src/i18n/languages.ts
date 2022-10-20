8ddec0ad49a89b6e60bf90ad6f5a25b37275b574
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2022-10-20 17:51:17 +0200

    implemented languages in articles

--- apps/website/src/i18n/languages.ts
+++ apps/website/src/i18n/languages.ts
@@ -1,21 +1,29 @@
-export default {
-	en: 'English',
-	de: 'Deutsch',
-	nl: 'Nederlands',
-	'pt-br': 'Português do Brasil',
-	fi: 'Suomi',
-	es: 'Español',
-	'zh-cn': '简体中文',
-	'zh-tw': '正體中文',
-	bg: 'Български',
-	fr: 'Français',
-	bn: 'বাংলা',
-	kr: '한국어',
-	ar: 'العربية',
-	da: 'Dansk',
-	ja: '日本語',
-	ru: 'Русский',
-	it: 'Italiano',
-	pl: 'Polish',
-	hu: 'Hungarian',
+import { SUPPORTED_LANGUAGES } from "@/constants";
+
+const allLanguages = {
+  en: "English",
+  de: "Deutsch",
+  nl: "Nederlands",
+  "pt-br": "Português do Brasil",
+  fi: "Suomi",
+  es: "Español",
+  "zh-cn": "简体中文",
+  "zh-tw": "正體中文",
+  bg: "Български",
+  fr: "Français",
+  bn: "বাংলা",
+  kr: "한국어",
+  ar: "العربية",
+  da: "Dansk",
+  ja: "日本語",
+  ru: "Русский",
+  it: "Italiano",
+  pl: "Polish",
+  hu: "Hungarian",
 };
+
+export default Object.fromEntries(
+  Object.entries(allLanguages).filter(([key]) =>
+    SUPPORTED_LANGUAGES.includes(key)
+  )
+);
