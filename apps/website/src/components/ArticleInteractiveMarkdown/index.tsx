74c932cba8790b0cf14be419ca3f65b2bc99a95d
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2022-10-29 20:58:06 +0200

    link icon fix

--- apps/website/src/components/ArticleInteractiveMarkdown/index.tsx
+++ apps/website/src/components/ArticleInteractiveMarkdown/index.tsx
@@ -38,6 +38,7 @@ const ArticleInteractiveMarkdown = () => {
         "http://www.w3.org/2000/svg",
         "svg"
       );
+
       const iconPath = document.createElementNS(
         "http://www.w3.org/2000/svg",
         "path"
@@ -50,19 +51,26 @@ const ArticleInteractiveMarkdown = () => {
         "M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z"
       );
       iconSvg.setAttribute("fill", "#e2e8f0");
-      iconSvg.setAttribute(
-        "style",
-        "position: absolute;left: -2rem;top:0.5rem;"
-      );
-      iconSvg.setAttribute("opacity", "0");
+      iconSvg.appendChild(iconPath);
+
+      const div = document.createElement("div");
+      div.appendChild(iconSvg);
+
+      div.style.opacity = "0";
+      div.style.width = "40px";
+      div.style.height = "100%";
+      div.style.position = "absolute" 
+      div.style.left = "-2.5rem" 
+      div.style.display = "flex" 
+      div.style.justifyContent = "center" 
+      div.style.alignItems = "center" 
 
       item.onmouseenter = () => {
-        iconSvg.setAttribute("opacity", "1");
+        div.style.opacity = "1";
       };
       item.onmouseleave = () => {
-        iconSvg.setAttribute("opacity", "0");
+        div.style.opacity = "0";
       };
-      iconSvg.appendChild(iconPath);
 
       const url = new URL(window.location.toString());
 
@@ -112,7 +120,7 @@ const ArticleInteractiveMarkdown = () => {
 
       tableList?.appendChild(li);
 
-      item.append(iconSvg);
+      item.append(div);
 
       // Make sure to prevent multiple ids being added to the URL
       // Add hover effect on title, then make an icon appear
