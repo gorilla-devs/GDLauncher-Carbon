bacc0e0461ae469cd5ac2665851ed4b64e39ed10
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2022-10-29 18:12:08 +0200

    download page styling fix

--- apps/website/src/components/DownloadSection/index.tsx
+++ apps/website/src/components/DownloadSection/index.tsx
@@ -22,17 +22,17 @@ const DownloadSection: Component<{ pathname: string }> = ({ pathname }) => {
         }
       >
         <div
-          class={`relative flex flex-col justify-center items-center pt-1 px-10 box-border h-screen bg-[url("https://https://edge.gdlauncher.com/assets/mc_forest_2.webp")] bg-no-repeat`}
+          class={`relative flex flex-col justify-center items-center pt-1 px-10 py-10 box-border z-10 bg-[url("https://https://edge.gdlauncher.com/assets/mc_forest_2.webp")] bg-no-repeat`}
         >
           <div class="absolute top-0 bottom-0 left-0 right-0">
             <div class="absolute top-0 bottom-0 left-0 right-0 z-10 opacity-95 bg-slate-800"></div>
           </div>
-          <div class="w-4/5 lg:w-full z-20 flex flex-col-reverse lg:flex-row justify-between items-center gap-10 lg:gap-20 max-w-7xl">
+          <div class="w-4/5 lg:w-full z-10 flex flex-col-reverse lg:flex-row justify-between items-center gap-10 lg:gap-12 max-w-7xl">
             <div class="py-0 px-4 max-w-2xl">
               <h3 class="text-5xl lg:text-8xl mb-2 font-bold">
                 {t("download.title")}
               </h3>
-              <p class="text-xl mb-12 font-light">{t("download.text")}</p>
+              <p class="text-xl mb-6 font-light z-20">{t("download.text")}</p>
               <Dropdown
                 pathname={pathname}
                 onclick={() => setIsDownloading(true)}
