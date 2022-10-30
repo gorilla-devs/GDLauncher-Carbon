f26aa422dae61ac7ebb982812ef286b438359e91
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2022-10-30 17:29:20 +0100

    fix seo article image preview

--- apps/website/src/utils/composeCDNAssetLink.ts
+++ apps/website/src/utils/composeCDNAssetLink.ts
@@ -3,3 +3,7 @@ import { APP_URLS } from "../constants";
 export default function composeCDNAssetLink(asset: string): string {
   return `${APP_URLS.cdn}/assets/${asset}`;
 }
+
+export function composeCDNArticleThumbnailAssetLink(slug: string, img: string): string {
+  return `${APP_URLS.cdn}/articles/${slug}/${img}`;
+}
