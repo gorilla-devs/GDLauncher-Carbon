2a8239edea465d4289b090bc00a48530279bfcbe
Davide <davide.ceschia@gmail.com>
2022-11-11 20:29:29 +0100

    wip on windows

--- packages/core/src/java/checker.rs
+++ packages/core/src/java/checker.rs
@@ -66,8 +66,58 @@ pub async fn find_java_paths() -> Vec<PathBuf> {
     }
 
     // Remove duplicates
-    javas = javas.into_iter().filter(|java_path| java_path.to_str().is_some()).collect();
-    javas.sort_by(|a, b| a.to_str().unwrap().to_lowercase().cmp(&b.to_str().unwrap().to_lowercase()));
+    javas = javas
+        .into_iter()
+        .filter(|java_path| java_path.to_str().is_some())
+        .collect();
+    javas.sort_by(|a, b| {
+        a.to_str()
+            .unwrap()
+            .to_lowercase()
+            .cmp(&b.to_str().unwrap().to_lowercase())
+    });
+    javas.dedup();
+
+    javas
+}
+
+#[cfg(target_os = "windows")]
+pub fn read_registry_key(key: &str, value: &str) -> Result<String> {
+    let hkcu = winreg::RegKey::predef(winreg::enums::HKEY_LOCAL_MACHINE);
+    let key = hkcu.open_subkey(key)?;
+    let res: String = key.get_value(value)?;
+    Ok(res)
+}
+
+#[cfg(target_os = "windows")]
+pub async fn find_java_paths() -> Vec<PathBuf> {
+    use winreg::enums::*;
+    use winreg::RegKey;
+    let mut javas: Vec<PathBuf> = vec![];
+    javas.push(PathBuf::from(get_default_java_path()));
+    let reg_paths = vec![
+        (r"SOFTWARE\JavaSoft\Java Runtime Environment", "JavaHome"),
+        (r"SOFTWARE\JavaSoft\Java Development Kit", "JavaHome"),
+        (r"SOFTWARE\JavaSoft\JRE", "JavaHome"),
+        (r"SOFTWARE\JavaSoft\JDK", "JavaHome"),
+    ];
+
+    let java_from_env = load_java_paths_from_env().await;
+    if let Ok(Some(java_from_env)) = java_from_env {
+        javas.extend(java_from_env);
+    }
+
+    // Remove duplicates
+    javas = javas
+        .into_iter()
+        .filter(|java_path| java_path.to_str().is_some())
+        .collect();
+    javas.sort_by(|a, b| {
+        a.to_str()
+            .unwrap()
+            .to_lowercase()
+            .cmp(&b.to_str().unwrap().to_lowercase())
+    });
     javas.dedup();
 
     javas
