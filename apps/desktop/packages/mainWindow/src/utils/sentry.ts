880da9c8ce1bae2b12d12efa0bb710f701d0dbaf
Davide <davide.ceschia@gmail.com>
2022-11-14 03:11:06 +0100

    fixes for windows

--- apps/desktop/packages/mainWindow/src/utils/sentry.ts
+++ apps/desktop/packages/mainWindow/src/utils/sentry.ts
@@ -4,21 +4,34 @@ import { RewriteFrames as RewriteFramesIntegration } from "@sentry/integrations"
 
 if (!import.meta.env.DEV) {
   try {
+    const removeLastSection = (url: string) => {
+      if (url.endsWith("/")) {
+        url = url.slice(0, -1);
+      }
+
+      let sections = url.split("/");
+
+      return sections.slice(0, sections.length - 1).join("/");
+    };
+
+    console.log("IMPORT META URL", import.meta.url);
     let basePath =
       "file://" +
-      window.nodePath.resolve(
-        import.meta.url.split("app.asar")[0].replace("file://", ""),
-        "../"
+      removeLastSection(import.meta.url.split("app.asar")[0]).replace(
+        "file://",
+        ""
       );
+    console.log("BASE PATH", basePath);
     Sentry.init({
       dsn: import.meta.env.VITE_SENTRY_DSN,
       integrations: [
         new BrowserTracing(),
         new RewriteFramesIntegration({
           iteratee: (frame) => {
-            console.log(frame);
             if (frame.filename) {
-              frame.filename = frame.filename.replace(basePath, "app:///");
+              console.log("FRAME", frame.filename);
+              frame.filename = frame.filename.replace(basePath, "app:/");
+              console.log("FRAME AFTER", frame.filename);
             }
             return frame;
           },
