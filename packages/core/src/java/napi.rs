5b373beb784cb589497d4288a1df9b272943243d
Davide <davide.ceschia@gmail.com>
2022-11-14 13:08:40 +0100

    test sqlite

--- packages/core/src/java/napi.rs
+++ packages/core/src/java/napi.rs
@@ -1,5 +1,11 @@
+use std::str::FromStr;
+
 use napi::bindgen_prelude::*;
 use napi_derive::napi;
+use sqlx::{
+    sqlite::{SqliteConnectOptions, SqliteJournalMode},
+    ConnectOptions, SqlitePool,
+};
 
 use super::{checker::find_java_paths, mc_java::fetch_java_manifest, JAVA_MANIFEST};
 
@@ -12,7 +18,14 @@ pub async fn init_java() -> Result<()> {
 
     let _ = JAVA_MANIFEST.set(java_manifest);
 
-    find_java_paths().await;
+    let pool = SqliteConnectOptions::from_str("sqlite://data/db.db")
+        .map_err(|err| Error::new(Status::GenericFailure, err.to_string()))?
+        .journal_mode(SqliteJournalMode::Wal)
+        .create_if_missing(true)
+        .read_only(false)
+        .connect()
+        .await
+        .map_err(|err| Error::new(Status::GenericFailure, err.to_string()))?;
 
     Ok(())
 }
