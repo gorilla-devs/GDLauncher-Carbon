6fa4f4da157f5961bb9a59d94ee37ce7c3e7e27d
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2022-11-24 18:11:22 +0100

    InstanceTIle text alignment

--- apps/desktop/packages/mainWindow/src/components/InstanceTile/index.tsx
+++ apps/desktop/packages/mainWindow/src/components/InstanceTile/index.tsx
@@ -6,9 +6,9 @@ type Props = {
 
 const InstanceTile = (props: Props) => {
   return (
-    <div class="flex flex-col justify-center items-center">
+    <div class="flex flex-col justify-center items-start">
       <div class="h-38 w-38 bg-green-600 rounded-2xl" />
-      <h4 class="m-2">{props.title}</h4>
+      <h4 class="my-2">{props.title}</h4>
       <div class="flex justify-between text-black-lightGray">
         <p class="m-0">{props.modloader}</p>
         <p class="m-0">{props.version}</p>
