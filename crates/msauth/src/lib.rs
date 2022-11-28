03799e4dd71fe744dbeac21ac731df843ddeb026
Davide <davide.ceschia@gmail.com>
2022-11-28 14:58:05 +0100

    wip on accounts

--- crates/msauth/src/lib.rs
+++ crates/msauth/src/lib.rs
@@ -1,8 +1,70 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+use std::{collections::HashSet, sync::Arc};
+use serde::{Deserialize, Serialize};
+
+use self::ms_auth::{McAuth, McProfile, MsAuth};
+
+mod ms_auth;
+
+#[derive(Debug, PartialEq, Eq, Hash, Clone, Serialize, Deserialize)]
+pub struct Account {
+    pub ms_data: MsAuth,
+    pub mc_data: McAuth,
+    pub mc_profile: McProfile,
+}
+
+/// Basic data structure for accounts
+#[derive(Debug, PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub struct Accounts {
+    pub inner: HashSet<Arc<Account>>,
+    selected_account: Option<Arc<Account>>,
+}
+
+impl Accounts {
+    pub fn new() -> Self {
+        Accounts {
+            inner: HashSet::new(),
+            selected_account: None,
+        }
+    }
+
+    pub async fn add_account(&mut self, account: Account) {
+        let account_ref = Arc::new(account);
+
+        self.inner.insert(account_ref.clone());
+        self.selected_account = Some(account_ref.clone());
+    }
+
+    pub async fn remove_account(&mut self, account: Arc<Account>) {
+        self.inner.remove(&account);
+
+        if self.selected_account == Some(account.clone()) {
+            // Assign next account if available
+            if let Some(account) = self.inner.iter().next() {
+                self.selected_account = Some(account.clone());
+            } else {
+                self.selected_account = None;
+            }
+        }
+    }
+
+    /// Verifies whether an account is still valid.
+    /// If it's not, it will try to refresh tokens.
+    /// If that fails, it will flag the account as expired.
+    // pub async fn validate_account(&self, account: Arc<Account>) -> Result<()> {
+    //     let azure_data = AZURE_DATA.lock().await;
+    //     if let Some(mut data) = (&*azure_data).clone() {
+    //         data.validate_token(&account.ms_data.id_token).await?;
+
+    //         return Ok(());
+    //     }
+    //     bail!("Azure data not initialized");
+    // }
+
+    pub fn get_selected_account(&self) -> Option<Arc<Account>> {
+        self.selected_account.clone()
+    }
+
+    pub fn get_accounts(&self) -> &HashSet<Arc<Account>> {
+        &self.inner
     }
 }
