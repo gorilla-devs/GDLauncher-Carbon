3c971915b427b325712b02773bb4dad72392f968
Davide <davide.ceschia@gmail.com>
2022-11-30 19:52:13 +0100

    Also lint ui

--- packages/ui/src/Select/index.tsx
+++ packages/ui/src/Select/index.tsx
@@ -1,4 +1,4 @@
-import { children, createSignal, createEffect } from "solid-js";
+import { children } from "solid-js";
 import styles from "./Select.module.scss";
 
 type Props = {
@@ -6,14 +6,6 @@ type Props = {
 };
 
 function Select(props: Props) {
-  const [q, setC] = createSignal(false);
-
-  createEffect(() => {
-    if (q()) {
-      console.log(q());
-    }
-  });
-
   const c = children(() => props.children);
   return <div class={styles.paolino}>{c()}</div>;
 }
