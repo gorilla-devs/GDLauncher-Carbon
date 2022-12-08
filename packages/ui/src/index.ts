f5f69b519e489558bd496e0be0eb0dcda0e56ced
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2022-12-08 14:35:40 +0100

    progressbar component

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -6,5 +6,6 @@ export { Pattern } from "./Pattern";
 export { CodeInput } from "./CodeInput";
 export { Carousel } from "./Carousel";
 export { InstanceTile } from "./InstanceTile";
+export { Progressbar } from "./Progressbar";
 
 import "virtual:uno.css";
