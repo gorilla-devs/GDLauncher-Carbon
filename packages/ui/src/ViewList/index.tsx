327fc89567291ae16d757f3b5557ee74eee96a27
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2022-12-11 13:44:40 +0100

    children

--- packages/ui/src/ViewList/index.tsx
+++ packages/ui/src/ViewList/index.tsx
@@ -1,24 +1,22 @@
-import { For, mergeProps } from "solid-js";
+import { mergeProps } from "solid-js";
 
 interface Props {
-  list: any[];
+  children: Element[];
   type?: "list" | "grid";
-  cols: number;
+  cols?: number;
 }
 
 function ViewList(props: Props) {
-  const mergedProps = mergeProps({ type: "grid" }, props);
+  const mergedProps = mergeProps({ type: "grid", cols: 4 }, props);
 
   return (
     <div
       class={`${mergedProps.type === "grid" ? `grid` : "flex flex-col"} gap-4`}
       style={{
-        "grid-template-columns": `repeat(${props.cols}, minmax(0, 1fr))`,
+        "grid-template-columns": `repeat(${mergedProps.cols}, minmax(0, 1fr))`,
       }}
     >
-      <For each={props.list}>
-        {() => <div class="bg-green-500 rounded-xl h-20" />}
-      </For>
+      {props.children}
     </div>
   );
 }
