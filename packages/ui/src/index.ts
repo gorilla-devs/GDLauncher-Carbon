ac26e6d22b4fdfd6bd7155b066dd1b36b88288cf
Gianmarco <31326058+Ladvace@users.noreply.github.com>
2022-12-12 23:39:18 +0100

    spinner component (#19)

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -6,6 +6,7 @@ export { Pattern } from "./Pattern";
 export { CodeInput } from "./CodeInput";
 export { Carousel } from "./Carousel";
 export { InstanceTile } from "./InstanceTile";
+export { Spinner } from "./Spinner";
 export { Notifications, addNotification } from "./NotificationManager";
 
 import "virtual:uno.css";
