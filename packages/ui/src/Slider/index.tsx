8508e96540f0030d89363f7ac3dae519b2caef29
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2022-12-15 14:04:09 +0100

    slider component

---
+++ packages/ui/src/Slider/index.tsx
@@ -0,0 +1,41 @@
+import { onMount } from "solid-js";
+import Style from "./Slider.module.scss";
+
+interface Props {
+  min: number;
+  max: number;
+  value: number;
+  // eslint-disable-next-line no-unused-vars
+  onChange?: (val: string) => void;
+}
+
+function Slider(props: Props) {
+  let ref: HTMLInputElement | undefined;
+
+  onMount(() => {
+    if (ref) {
+      ref.style.setProperty("--value", ref.value);
+      props?.onChange?.(ref.value);
+      ref.style.setProperty("--min", ref.min == "" ? "0" : ref.min);
+      ref.style.setProperty("--max", ref.max == "" ? "100" : ref.max);
+      ref.addEventListener("input", () =>
+        ref?.style.setProperty("--value", ref.value)
+      );
+    }
+  });
+  return (
+    <input
+      ref={ref}
+      class={`${Style.slider} ${Style.sliderProgress} w-full`}
+      style={{
+        "--value": props.value,
+        "--min": props.min || 0,
+        "--max": props.max || 100,
+      }}
+      type="range"
+      value="0"
+    />
+  );
+}
+
+export { Slider };
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -6,6 +6,7 @@ export { Pattern } from "./Pattern";
 export { CodeInput } from "./CodeInput";
 export { Carousel } from "./Carousel";
 export { InstanceTile } from "./InstanceTile";
+export { Slider } from "./Slider";
 export { News } from "./News";
 export { Tabs } from "./Tabs";
 export { Progressbar } from "./Progressbar";
