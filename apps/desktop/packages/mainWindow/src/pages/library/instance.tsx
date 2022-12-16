d593eaaaae295b9fa66ad525a0bf4b501aa6a43e
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2022-12-16 16:54:41 +0100

    wip instance manager overview

--- apps/desktop/packages/mainWindow/src/pages/library/instance.tsx
+++ apps/desktop/packages/mainWindow/src/pages/library/instance.tsx
@@ -1,3 +1,4 @@
+import { Button, Tabs } from "@gd/ui";
 import { useNavigate, useParams } from "@solidjs/router";
 import headerMockImage from "/assets/images/minecraft-forge.jpg";
 
@@ -15,8 +16,30 @@ const Instace = () => {
         }}
       >
         <div class="mt-64 h-200 bg-black-black">
-          <div class="mt-65">instace {params.id}</div>
-          <button onClick={() => navigate("/library")}>back</button>
+          <div class="h-65 absolute top-0 left-0">
+            <Button
+              icon={<div class="i-ri:arrow-drop-left-line" />}
+              size="small"
+            >
+              Back
+            </Button>
+            <button onClick={() => navigate("/library")}>back</button>
+          </div>
+          <div class="mt-65 px-6">
+            <Tabs
+              tabs={[
+                {
+                  name: "mods",
+                  component: <div>mods jsx</div>,
+                  icon: "image-2-fill",
+                },
+                {
+                  name: "modpacks",
+                  component: <div>modpacks jsx</div>,
+                },
+              ]}
+            />
+          </div>
         </div>
       </div>
     </>
