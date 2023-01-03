1d64d8c32030d2ec4551a802003cb4f44d20e2f1
Davide <davide.ceschia@gmail.com>
2023-01-03 02:39:30 +0100

    rollback pnpm lock

--- apps/desktop/packages/mainWindow/src/utils/rspcClient.ts
+++ apps/desktop/packages/mainWindow/src/utils/rspcClient.ts
@@ -5,13 +5,13 @@ import { createSolidQueryHooks } from "@rspc/solid";
 import type { Procedures } from "@gd/carbon_core"; // These were the bindings exported from your Rust code!
 
 export const client = createClient<Procedures>({
-  // transport: new WebsocketTransport("ws://localhost:4000/rspc/ws"),
-  transport: new FetchTransport("http://localhost:4000/rspc"),
+  transport: new WebsocketTransport("ws://localhost:4000/rspc/ws"),
+  // transport: new FetchTransport("http://localhost:4000/rspc"),
 });
 
 export const queryClient = new QueryClient();
 export const rspc = createSolidQueryHooks<Procedures>();
 
-// client.addSubscription(["pings", []], {
-//   onData: (e) => console.log(e),
-// });
+client.addSubscription(["pings"], {
+  onData: (e) => console.log(e),
+});
