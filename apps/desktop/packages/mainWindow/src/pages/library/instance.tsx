e8fbc43389002461919c91670eff1190bd385c4f
Gianmarco <31326058+Ladvace@users.noreply.github.com>
2023-01-03 16:34:35 +0100

    parallax effect instance manager (#26)

--- apps/desktop/packages/mainWindow/src/pages/library/instance.tsx
+++ apps/desktop/packages/mainWindow/src/pages/library/instance.tsx
@@ -1,14 +1,25 @@
 import { useNavigate, useParams } from "@solidjs/router";
+import headerMockImage from "/assets/images/minecraft-forge.jpg";
 
 const Instace = () => {
   const params = useParams();
   const navigate = useNavigate();
 
   return (
-    <div>
-      <div>instace {params.id}</div>
-      <button onClick={() => navigate("/library")}>back</button>
-    </div>
+    <>
+      <div
+        class="relative h-full bg-fixed bg-no-repeat max-h-full overflow-auto"
+        style={{
+          "background-image": `url("${headerMockImage}")`,
+          "background-position": "center -5rem",
+        }}
+      >
+        <div class="mt-64 h-200 bg-black-black">
+          <div class="mt-65">instace {params.id}</div>
+          <button onClick={() => navigate("/library")}>back</button>
+        </div>
+      </div>
+    </>
   );
 };
 
