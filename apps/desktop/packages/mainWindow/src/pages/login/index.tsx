a885ce3e790576946fc50b26c435a7cc0c8b2090
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2023-01-11 13:39:12 +0100

    linting fix

--- apps/desktop/packages/mainWindow/src/pages/login/index.tsx
+++ apps/desktop/packages/mainWindow/src/pages/login/index.tsx
@@ -8,7 +8,7 @@ import CodeStep from "./CodeStep";
 export default function Login() {
   const [step, setStep] = createSignal(0);
   const [deviceCodeObject, setDeviceCodeObject] = createSignal<any>(null);
-  const [t, { add, locale }] = useI18n();
+  const [_, { add, locale }] = useI18n();
 
   return (
     <div class="flex justify-center items-center w-full h-full bg-image-loginBG.jpg p-0">
