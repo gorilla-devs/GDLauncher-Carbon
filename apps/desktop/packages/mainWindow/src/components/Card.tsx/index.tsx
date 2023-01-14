1ee089d99a4a077f64b5732630c5a881071e6b25
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2023-01-14 14:28:31 +0100

    overview tab instance manager

---
+++ apps/desktop/packages/mainWindow/src/components/Card.tsx/index.tsx
@@ -0,0 +1,56 @@
+import { mergeProps } from "solid-js";
+import vanillaIcon from "/assets/images/icons/vanilla.png";
+import bookIcon from "/assets/images/icons/book.png";
+import clockIcon from "/assets/images/icons/clock.png";
+import pickAxeIcon from "/assets/images/icons/pickaxe.png";
+import signIcon from "/assets/images/icons/sign.png";
+import cartIcon from "/assets/images/icons/cart.png";
+
+type Icon = "vanilla" | "book" | "cart" | "clock" | "pickaxe" | "sign";
+
+interface Props {
+  icon?: Icon;
+  title: string;
+  text: string;
+}
+
+const Card = (props: Props) => {
+  const mergedProps = mergeProps({ title: "", text: "" }, props);
+
+  const getIcon = (icon: Icon) => {
+    switch (icon) {
+      case "vanilla":
+        return vanillaIcon;
+      case "book":
+        return bookIcon;
+      case "cart":
+        return cartIcon;
+      case "clock":
+        return clockIcon;
+      case "pickaxe":
+        return pickAxeIcon;
+      case "sign":
+        return signIcon;
+      default:
+        return vanillaIcon;
+    }
+  };
+
+  return (
+    <div class="flex items-center justify-between p-5 h-23 w-59 bg-shade-7 rounded-xl box-border">
+      <div class="h-13 w-13 bg-shade-8 rounded-lg flex justify-center items-center">
+        <img src={getIcon(props.icon || "vanilla")} />
+      </div>
+      <div>
+        <h5 class="text-shade-0 uppercase font-medium m-0">
+          {mergedProps.title}
+        </h5>
+        <p class="text-white font-bold uppercase text-2xl m-0">
+          {mergedProps.text}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- apps/desktop/packages/mainWindow/src/pages/Library/Instance/Overview.tsx
+++ apps/desktop/packages/mainWindow/src/pages/Library/Instance/Overview.tsx
@@ -1,5 +1,27 @@
+import Card from "@/components/Card.tsx";
+
 const Overview = () => {
-  return <div>Overview</div>;
+  return (
+    <div class="flex flex-col gap-4 max-w-185">
+      <div class="w-full flex justify-center flex-wrap gap-4">
+        <Card title="Minecraft version" text="1.19.2" icon="vanilla" />
+        <Card title="Minecraft version" text="1.19.2" icon="book" />
+        <Card title="Minecraft version" text="1.19.2" icon="pickaxe" />
+        <Card title="Minecraft version" text="1.19.2" icon="cart" />
+        <Card title="Minecraft version" text="1.19.2" icon="clock" />
+        <Card title="Minecraft version" text="1.19.2" icon="sign" />
+      </div>
+      <div class="flex flex-col items-start justify-between gap-2 p-5 w-59 bg-shade-7 rounded-xl box-border w-full">
+        <div class="text-shade-0 uppercase">Notes</div>
+        <p class="m-0 text-sm leading-6">
+          Minecraft Forge is a handy place to store, sort, and keep tabs on all
+          your mods. If you’re after more inspiration, our guide to the best
+          Minecraft shaders and Minecraft texture packs add plenty of visual
+          flair to the blocky universe. Not sure what to build in game?
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Overview;
