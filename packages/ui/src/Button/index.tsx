bd7a6692d487b40e85bb53427a50f6390c1ac979
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2023-01-15 20:58:52 +0100

    removed unused code

--- packages/ui/src/Button/index.tsx
+++ packages/ui/src/Button/index.tsx
@@ -168,11 +168,6 @@ function Button(props: Props) {
           {c()}
         </Show>
       </button>
-      {/* <Show when={props.loading}>
-        <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-          <Spinner />
-        </div>
-      </Show> */}
     </div>
   );
 }
