0a4296334172ef63311ddf505e0e0f82fadb5947
Davide <davide.ceschia@gmail.com>
2023-01-18 02:45:40 +0100

    wip on mc/javas apis

--- crates/carbon_bindings/src/api/java.rs
+++ crates/carbon_bindings/src/api/java.rs
@@ -5,19 +5,52 @@ use std::path::PathBuf;
 
 #[derive(Type, Serialize)]
 struct Java {
+    id: String,
     name: String,
+    version: String,
     path: PathBuf,
+    default: bool,
 }
+
+#[derive(Type, Serialize)]
+struct Javas(Vec<Java>);
 
 pub(super) fn mount() -> impl RouterBuilderLike<()> {
     Router::new().query("getAvailableJavas", |t| {
         t(|ctx: (), _args: ()| async move {
-            // let javas = ctx.java_manager.get_available_javas().await?;
-            let java = Java {
+            let mut javas = Vec::new();
+            javas.push(Java {
+                id: "java-8".to_string(),
                 name: "Java 8".to_string(),
+                version: "1.8.0_275".to_string(),
                 path: PathBuf::from("/usr/lib/jvm/java-8-openjdk-amd64"),
-            };
-            Ok(java)
+                default: true,
+            });
+            javas.push(Java {
+                id: "java-11".to_string(),
+                name: "Java 11".to_string(),
+                version: "1.11.0_33".to_string(),
+                path: PathBuf::from("/usr/lib/jvm/java-11-openjdk-amd64"),
+                default: true,
+            });
+            javas.push(Java {
+                id: "java-11".to_string(),
+                name: "Java 11".to_string(),
+                version: "1.11.0_35".to_string(),
+                path: PathBuf::from("/usr/lib/jvm/java-11_35-openjdk-amd64"),
+                default: false,
+            });
+            javas.push(Java {
+                id: "java-16".to_string(),
+                name: "Java 16".to_string(),
+                version: "1.16.0_33".to_string(),
+                path: PathBuf::from("/usr/lib/jvm/java-16-openjdk-amd64"),
+                default: false,
+            });
+
+            let final_javas = Javas(javas);
+
+            Ok(final_javas)
         })
     })
 }
--- crates/carbon_bindings/src/api/mc.rs
+++ crates/carbon_bindings/src/api/mc.rs
@@ -1,23 +1,68 @@
 use super::Ctx;
 use rspc::{Router, RouterBuilderLike, Type};
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
 use std::path::PathBuf;
 
 #[derive(Type, Serialize)]
-struct Java {
+struct Instance {
+    id: String,
     name: String,
-    path: PathBuf,
 }
 
+#[derive(Type, Serialize)]
+struct Instances(Vec<Instance>);
+
 pub(super) fn mount() -> impl RouterBuilderLike<()> {
-    Router::new().query("getAvailableJavas", |t| {
-        t(|ctx: (), _args: ()| async move {
-            // let javas = ctx.java_manager.get_available_javas().await?;
-            let java = Java {
-                name: "Java 8".to_string(),
-                path: PathBuf::from("/usr/lib/jvm/java-8-openjdk-amd64"),
-            };
-            Ok(java)
+    Router::new()
+        .query("getInstances", |t| {
+            t(|ctx: (), _args: ()| async move {
+                let mut instances = Vec::new();
+                instances.push(Instance {
+                    id: "88r39459345939453".to_string(),
+                    name: "My first instance".to_string(),
+                });
+                instances.push(Instance {
+                    id: "88r39459345939456".to_string(),
+                    name: "My second instance".to_string(),
+                });
+                instances.push(Instance {
+                    id: "88r39459345939451".to_string(),
+                    name: "Instance with a very long name".to_string(),
+                });
+                instances.push(Instance {
+                    id: "88r39459345336457".to_string(),
+                    name: "Vanilla Minecraft".to_string(),
+                });
+                instances.push(Instance {
+                    id: "84439459345336457".to_string(),
+                    name: "Forge Minecraft".to_string(),
+                });
+                instances.push(Instance {
+                    id: "82h39459345336457".to_string(),
+                    name: "All The Mods 6".to_string(),
+                });
+
+                let final_instances = Instances(instances);
+
+                Ok(final_instances)
+            })
         })
-    })
+        .query("getInstance", |t| {
+            t(|ctx: (), args: String| async move {
+                let instance = Instance {
+                    id: "82h39459345336457".to_string(),
+                    name: "All The Mods 6".to_string(),
+                };
+
+                Ok(instance)
+            })
+        })
+        .mutation("updateInstanceName", |t| {
+            #[derive(Type, Deserialize)]
+            struct Args {
+                id: String,
+                new_name: String,
+            }
+            t(|_, args: Args| {})
+        })
 }
