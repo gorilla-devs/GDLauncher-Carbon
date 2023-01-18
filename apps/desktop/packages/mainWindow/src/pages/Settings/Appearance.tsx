0e0e672e80a8b3de8f4360eef973ad1f4422d6d8
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2023-01-18 14:15:06 +0100

    fix color preview and left tab

--- apps/desktop/packages/mainWindow/src/pages/Settings/Appearance.tsx
+++ apps/desktop/packages/mainWindow/src/pages/Settings/Appearance.tsx
@@ -3,13 +3,13 @@ import { setTheme } from "@/utils/themeManager";
 
 const Appearance = () => {
   return (
-    <div class="flex py-5 px-6">
-      <div class="flex gap-4">
-        <div class="w-50 p-1 bg-shade-9 flex justify-center items-center">
+    <div class="py-5 px-6 flex justify-center">
+      <div class="flex justify-between border-box w-full max-w-[35rem]">
+        <div class="flex flex-col w-42 p-1 bg-[#15181E] flex justify-center items-center">
           <ThemePreview
-            shade1="fill-shade-9"
-            shade2="fill-shade-7"
-            shade3="fill-shade-6"
+            shade1="fill-[#15181E]"
+            shade2="fill-[#272B35]"
+            shade3="fill-[#333947]"
           />
           <input
             type="radio"
@@ -19,8 +19,12 @@ const Appearance = () => {
             }}
           />
         </div>
-        <div class="w-50 p-1 bg-shade-9 flex justify-center items-center">
-          <ThemePreview />
+        <div class="flex flex-col w-42 p-1 bg-shade-9 flex justify-center items-center">
+          <ThemePreview
+            shade1="fill-[#380505]"
+            shade2="fill-[#A90F0F]"
+            shade3="fill-[#E11313]"
+          />
           <input
             type="radio"
             name="radio-theme"
@@ -29,8 +33,12 @@ const Appearance = () => {
             }}
           />
         </div>
-        <div class="w-50 p-1 bg-shade-9 flex justify-center items-center">
-          <ThemePreview />
+        <div class="flex flex-col w-42 p-1 bg-shade-9 flex justify-center items-center">
+          <ThemePreview
+            shade1="fill-shade-9"
+            shade2="fill-shade-7"
+            shade3="fill-shade-6"
+          />
           <input type="radio" name="radio-theme" />
         </div>
       </div>
