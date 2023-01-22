6d7e6bf4640b6fdfa1f21f6e385ffed739c9596b
Davide <davide.ceschia@gmail.com>
2023-01-22 18:13:23 +0100

    fix

--- packages/config/src/appVersion.ts
+++ packages/config/src/appVersion.ts
@@ -4,7 +4,7 @@ const execSync = require("child_process").execSync;
 const gitHash = execSync("git rev-parse --short HEAD").toString().trim();
 
 const BASE_VERSION = JSON.parse(
-  fs.readFileSync(path.join(__dirname, "version.json"))
+  fs.readFileSync(path.resolve(__dirname, "../", "version.json"))
 ).version;
 
 export const COMPLETE_VERSION = `${BASE_VERSION}+${gitHash}`;
