77582b58932c9837620c4aa809b2aa287524cc04
Lucio Gabriele Pisanello <luciogabriele.pisanello@gmail.com>
2023-01-23 04:46:23 +0100

    * finished some fixes on code generators integration on pipe

--- crates/carbon_minecraft/src/app/configuration.rs
+++ crates/carbon_minecraft/src/app/configuration.rs
@@ -1,15 +1,37 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Ord, PartialOrd, PartialEq, Eq)]
-pub(crate) struct AppConfiguration {
-    id: i32,
+pub(crate) struct AppConfiguration<'a> {
+    _id: i32,
+    default_db_url: & 'a str
+}
+
+#[cfg(not(test))]
+impl <'a> Default for AppConfiguration<'a>{
+    fn default() -> Self {
+        AppConfiguration{
+            _id: 0,
+            default_db_url: "./dev.db"
+        }
+    }
+}
+
+#[cfg(test)]
+impl <'a> Default for AppConfiguration<'a>{
+    fn default() -> Self {
+        AppConfiguration{
+            _id: 0,
+            default_db_url: "./prisma/dev.db"
+        }
+    }
 }
 
 
 #[cfg(test)]
 mod unit_tests {
+    use std::path::PathBuf;
+    use crate::app::configuration::AppConfiguration;
     use crate::db;
-    use crate::db::app_configuration;
     use crate::db::app_configuration::SetParam::SetId;
     use crate::db::app_configuration::WhereParam;
     use crate::db::read_filters::IntFilter;
@@ -19,8 +41,14 @@ mod unit_tests {
     #[tracing_test::traced_test]
     async fn persistence_ok() {
 
-        let client = db::new_client().await
-            .expect("unable to build app_configuration client");
+        let default_configuration = &AppConfiguration::default();
+        let db_url = PathBuf::from(default_configuration.default_db_url);
+        let db_url = &db_url.canonicalize().unwrap();
+        let db_url = db_url.to_str().expect("db url is: <<unrepresentable path>>");
+        let db_url = format!("file:{}", db_url).as_str().to_string();
+
+        let client = db::new_client_with_url(db_url.as_str()).await
+            .expect(format!("unable to build app_configuration client using db_url : {db_url}").as_str());
 
         let configuration = client
             .app_configuration()
@@ -29,10 +57,10 @@ mod unit_tests {
             .await
             .expect("unable to exec create query for app_configuration");
 
-        let serialized_configuration = serde_json::to_string_pretty(&configuration)
+        let _serialized_configuration = serde_json::to_string_pretty(&configuration)
             .expect("unable to serialize app_configuration");
 
-        let count = client.app_configuration()
+        let _count = client.app_configuration()
             .count(vec![WhereParam::Id(IntFilter::Equals(0))])
             .exec().await
             .expect("unable to select app_configuration");
