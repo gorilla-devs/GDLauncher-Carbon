8e7054080d3d4c22672b0fa6858ad73cfd7538fe
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2023-01-26 14:44:44 +0100

    other settings

--- apps/desktop/packages/mainWindow/src/pages/Settings/General.tsx
+++ apps/desktop/packages/mainWindow/src/pages/Settings/General.tsx
@@ -43,10 +43,23 @@ const General = () => {
         </div>
       </div>
       <div class="mb-6">
-        <h5 class="mt-0 mb-2">Instance Sorting</h5>
+        <h5 class="mt-0 mb-2">
+          <Trans
+            key="instance_sorting"
+            options={{
+              defaultValue: "Instance Sorting",
+            }}
+          />
+        </h5>
         <div class="flex w-full justify-between">
           <p class="text-shade-3 max-w-96 m-0">
-            Select the method in which instances should be sorted.
+            <Trans
+              key="select_instance_sorting_method"
+              options={{
+                defaultValue:
+                  "Select the method in which instances should be sorted.",
+              }}
+            />
           </p>
           <Dropdown
             value="en"
@@ -58,36 +71,78 @@ const General = () => {
         </div>
       </div>
       <div class="mb-6">
-        <h5 class="mt-0 mb-2">Expert user mod</h5>
+        <h5 class="mt-0 mb-2">
+          <Trans
+            key="expert_user_mod"
+            options={{
+              defaultValue: "Expert user mod",
+            }}
+          />
+        </h5>
         <div class="flex w-full justify-between">
           <p class="text-shade-3 max-w-96 m-0">
-            Adds more control over the settings of your instances and java.
+            <Trans
+              key="settings_more_control"
+              options={{
+                defaultValue:
+                  "Adds more control over the settings of your instances and java.",
+              }}
+            />
           </p>
           <Switch checked={true} />
         </div>
       </div>
       <div class="mb-6">
-        <h5 class="mt-0 mb-2">Hide launcher while playing</h5>
+        <h5 class="mt-0 mb-2">
+          <Trans
+            key="settings_hide_launcher_playing_title"
+            options={{
+              defaultValue: "Hide launcher while playing",
+            }}
+          />
+        </h5>
         <div class="flex w-full justify-between">
           <p class="text-shade-3 max-w-96 m-0">
-            Automatically hide the launcher when launching an instance. You will
-            still be able to open it from the icon tray.
+            <Trans
+              key="settings_hide_launcher_playing_text"
+              options={{
+                defaultValue:
+                  "Automatically hide the launcher when launching an instance. You will still be able to open it from the icon tray.",
+              }}
+            />
           </p>
           <Switch checked={false} />
         </div>
       </div>
       <div class="mb-6 max">
-        <h5 class="mt-0 mb-2">Potato PC mode</h5>
+        <h5 class="mt-0 mb-2">
+          <Trans
+            key="settings_potatp_mode_title"
+            options={{
+              defaultValue: "Potato PC mode",
+            }}
+          />
+        </h5>
         <div class="flex w-full justify-between">
           <p class="text-shade-3 max-w-96 m-0">
-            You got a potato PC? Don't worry! We got you covered. Enable this
-            and all animations and special effects will be disabled.
+            <Trans
+              key="settings_potatp_mode_text"
+              options={{
+                defaultValue:
+                  "You got a potato PC? Don't worry! We got you covered. Enable this and all animations and special effects will be disabled.",
+              }}
+            />
           </p>
           <Switch checked={false} />
         </div>
       </div>
       <Button rounded={false} variant="secondary" textColor="text-red">
-        Reset all to default
+        <Trans
+          key="settings_reset"
+          options={{
+            defaultValue: "Reset all to default",
+          }}
+        />
       </Button>
       <div class="mt-16 flex gap-24 mb-6">
         <div class="flex gap-5">
@@ -98,8 +153,13 @@ const General = () => {
           </div>
         </div>
         <p class="text-shade-5 m-0">
-          You're currently on the latest version. We automatically check for
-          updates and we will inform you whenever one is available.
+          <Trans
+            key="settings_last_version"
+            options={{
+              defaultValue:
+                "You're currently on the latest version. We automatically check for updates and we will inform you whenever one is available.",
+            }}
+          />
         </p>
       </div>
     </div>
