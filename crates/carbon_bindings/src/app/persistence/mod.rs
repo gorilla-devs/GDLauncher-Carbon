03e1097af6f270a04c62105adddc62367ba7b727
Lucio Gabriele Pisanello <luciogabriele.pisanello@gmail.com>
2023-01-27 17:31:22 +0100

    * exposed theme set/get api via rspc router (#48)
    * fixed db_client initializing flow

--- crates/carbon_bindings/src/app/persistence/mod.rs
+++ crates/carbon_bindings/src/app/persistence/mod.rs
@@ -4,33 +4,32 @@ use prisma_client_rust::NewClientError;
 use thiserror::Error;
 use tokio::sync::RwLock;
 use crate::app::App;
+use crate::db;
 use crate::db::PrismaClient;
 
 #[derive(Error, Debug)]
-pub enum  PersistenceManagerError{
+pub enum PersistenceManagerError {
     #[error("error raised while trying to build the client for DB : {0}")]
     ClientError(#[from]NewClientError)
 }
 
-#[derive(Default, Clone)]
-pub(crate) struct PersistenceManager{
-    app: Weak<RwLock<App>>
+pub(crate) struct PersistenceManager {
+    app: Weak<RwLock<App>>,
+    db_client: Arc<RwLock<PrismaClient>>,
 }
 
-impl PersistenceManager{
-
-    pub fn make_for_app(app: &Arc<RwLock<App>>) -> PersistenceManager{
-        PersistenceManager{
+impl PersistenceManager {
+    pub async fn make_for_app(app: &Arc<RwLock<App>>) -> PersistenceManager {
+        let db_client = db::new_client().await.expect("unable to connect to db!");
+        PersistenceManager {
             app: Arc::downgrade(app),
+            db_client: Arc::new(RwLock::new(db_client)),
         }
     }
 
-    pub async fn get_db_client(&self) -> Result<PrismaClient, PersistenceManagerError>{
+    pub async fn get_db_client(&self) -> Arc<RwLock<PrismaClient>> {
         trace!("retrieving db client");
-        let client = crate::db::new_client().await?;
-        trace!("db client correctly retrieved");
-        Ok(client)
+        self.db_client.clone()
     }
-
 }
 
