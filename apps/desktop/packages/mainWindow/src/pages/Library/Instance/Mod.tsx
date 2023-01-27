989213c68f17e93f848be834b3d6715ea9086cf4
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2023-01-27 21:50:00 +0100

    translation mod tab

--- apps/desktop/packages/mainWindow/src/pages/Library/Instance/Mod.tsx
+++ apps/desktop/packages/mainWindow/src/pages/Library/Instance/Mod.tsx
@@ -29,17 +29,19 @@ const getIcon = (modloader: Modloader) => {
 const Mod = (props: Props) => {
   return (
     <div class="w-full h-14 flex items-center py-2 box-border">
-      <div class="flex gap-4 items-center">
-        <Checkbox checked={true} disabled={false} />
-        <div class="flex items-center gap-2">
-          <div class="bg-green-500 h-10 w-10 rounded-xl" />
-          <div class="flex flex-col">
-            {props.mod.title}
-            <div class="flex gap-2">
-              <img class="w-4 h-4" src={getIcon(props.mod.modloader)} />
-              <p class="m-0 text-shade-5 text-sm">
-                {`${props.mod.modloader} ${props.mod.mcversion}`}
-              </p>
+      <div class="flex gap-4 justify-between items-center w-full">
+        <div class="flex gap-4 justify-between items-center">
+          <Checkbox checked={true} disabled={false} />
+          <div class="flex items-center gap-2">
+            <div class="bg-green-500 h-10 w-10 rounded-xl" />
+            <div class="flex flex-col">
+              {props.mod.title}
+              <div class="flex gap-2">
+                <img class="w-4 h-4" src={getIcon(props.mod.modloader)} />
+                <p class="m-0 text-shade-5 text-sm">
+                  {`${props.mod.modloader} ${props.mod.mcversion}`}
+                </p>
+              </div>
             </div>
           </div>
         </div>
