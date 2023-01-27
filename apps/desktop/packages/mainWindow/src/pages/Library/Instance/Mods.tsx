be1dcab7c9f572b904e9ec536d313ac27e301087
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2023-01-27 17:54:55 +0100

    button color fix and dropDown onBlur fix

--- apps/desktop/packages/mainWindow/src/pages/Library/Instance/Mods.tsx
+++ apps/desktop/packages/mainWindow/src/pages/Library/Instance/Mods.tsx
@@ -1,5 +1,44 @@
+import { Trans } from "@gd/i18n";
+import { Button, Dropdown, Input } from "@gd/ui";
+
 const Mods = () => {
-  return <div />;
+  return (
+    <div class="flex flex-col gap-4 max-w-185">
+      <div class="flex">
+        <Input
+          placeholder="Type Here"
+          icon={<div class="i-ri:search-line" />}
+          class="w-full rounded-full text-shade-0"
+        />
+        <div class="flex items-center gap-4">
+          <p class="m-0 text-shade-5">
+            <Trans
+              key="sort_by"
+              options={{
+                defaultValue: "Sort by:",
+              }}
+            />
+          </p>
+          <Dropdown
+            rounded
+            options={[
+              { label: "A to Z", key: "asc" },
+              { label: "Z to A", key: "desc" },
+            ]}
+            value="asc"
+          />
+          <Button variant="outline">
+            <Trans
+              key="add_mod"
+              options={{
+                defaultValue: "+ Add Mod",
+              }}
+            />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Mods;
--- packages/ui/src/Button/index.tsx
+++ packages/ui/src/Button/index.tsx
@@ -82,7 +82,7 @@ const getVariant = (
     outline: {
       ...commonStyle,
       "border-2": true,
-      "text-shade-7": !isDisabled,
+      "text-white": !isDisabled,
       "text-shade-5": isDisabled,
       "border-white": !isDisabled,
       "border-shade-5": isDisabled,
