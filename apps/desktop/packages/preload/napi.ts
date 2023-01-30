92fdf941713b2e2fe3a6c2e24a73e5e190427f33
Davide <davide.ceschia@gmail.com>
2023-01-31 00:40:13 +0100

    fix ci?

--- apps/desktop/packages/preload/napi.ts
+++ apps/desktop/packages/preload/napi.ts
@@ -3,9 +3,9 @@ import path from "path";
 
 const isDev = import.meta.env.MODE === "development";
 
-const prodPath = process.platform === "darwin" ? "../../../" : "../../";
+// const prodPath = process.platform === "darwin" ? "../../../" : "../../";
 
-const nAPIPath = isDev ? "../../../../packages/native_interface" : prodPath;
+const nAPIPath = isDev ? "../../../../packages/native_interface" : "../../../";
 
 let napiLoaded = new Promise((resolve, reject) => {
   import(path.resolve(__dirname, nAPIPath, "core.node"))
