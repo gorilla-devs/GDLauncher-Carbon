9893957d6a69178dce9bc67ce87fcf292a970535
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2023-02-06 18:40:56 +0100

    wip slider

--- packages/ui/src/Slider/index.tsx
+++ packages/ui/src/Slider/index.tsx
@@ -1,40 +1,104 @@
-import { onMount } from "solid-js";
+import { For, createEffect, createSignal, onCleanup, onMount } from "solid-js";
 import Style from "./Slider.module.scss";
 
+interface Marks {
+  [mark: number]: string;
+}
 export interface Props {
   min: number;
   max: number;
   value: number;
-  // eslint-disable-next-line no-unused-vars
-  onChange?: (val: string | undefined) => void;
+  marks: Marks;
+  defaultValue: number;
+  onChange?: (_val: string | undefined) => void;
 }
 
 function Slider(props: Props) {
-  let ref: HTMLInputElement | undefined;
+  const [xElem, setXElem] = createSignal<number>(0);
+  const [xPos, setXPos] = createSignal<number>(0);
+  const [newPos, setNewPos] = createSignal<number>(0);
+  const [dragging, setDragging] = createSignal(false);
+
+  let handleRef: HTMLDivElement;
+  let sliderRef: HTMLDivElement;
+
+  createEffect(() => {
+    setNewPos(xPos() - xElem());
+  });
+
+  const mousedown = (e: MouseEvent) => {
+    e.preventDefault();
+    setDragging(true);
+    setXElem(xPos() - handleRef.offsetLeft);
+  };
+
+  const mousemove = (e: MouseEvent) => {
+    setXPos(e.pageX);
+
+    if (dragging()) {
+      const offsetWidth = sliderRef.offsetWidth;
+
+      if (newPos() < 0) {
+        setNewPos(0);
+      }
+
+      if (newPos() > offsetWidth) {
+        setNewPos(offsetWidth);
+      }
+
+      // if (newPos % 10 === 0) {
+      //   handleRef.style.left = newPos + "px";
+      // }
+      handleRef.style.transform = newPos() + "px";
+    }
+  };
+
+  const mouseup = () => {
+    setDragging(false);
+  };
 
   onMount(() => {
-    if (ref) {
-      ref.style.setProperty("--value", ref.value);
-      ref.style.setProperty("--min", ref.min == "" ? "0" : ref.min);
-      ref.style.setProperty("--max", ref.max == "" ? "100" : ref.max);
-      ref.addEventListener("input", () => {
-        props?.onChange?.(ref?.value);
-        ref?.style.setProperty("--value", ref.value);
-      });
-    }
+    handleRef.addEventListener("mousedown", mousedown);
+    document.addEventListener("mousemove", mousemove);
+    document.addEventListener("mouseup", mouseup);
   });
+
+  onCleanup(() => {
+    handleRef.removeEventListener("mousedown", mousedown);
+    document.removeEventListener("mousemove", mousemove);
+    document.removeEventListener("mouseup", mouseup);
+  });
+
   return (
-    <input
-      ref={ref}
-      class={`${Style.slider} ${Style.sliderProgress} w-full`}
-      style={{
-        "--value": props.value,
-        "--min": props.min || 0,
-        "--max": props.max || 100,
-      }}
-      type="range"
-      value="0"
-    />
+    <div class="relative">
+      <For each={Object.entries(props.marks)}>
+        {([value]) => (
+          <div
+            class="w-2 h-2 bg-primary rounded-full border-4 border-solid border-primary -top-1"
+            style={{
+              position: "absolute",
+              left: `${value}%`,
+            }}
+          />
+        )}
+      </For>
+      <div
+        ref={(el) => {
+          handleRef = el;
+        }}
+        class="w-4 h-4 bg-shade-8 rounded-full border-4 border-solid border-primary -top-2 cursor-move"
+        style={{
+          position: "absolute",
+          left: `${props.defaultValue}px`,
+        }}
+      />
+      <div
+        ref={(el) => {
+          sliderRef = el;
+        }}
+        class={`${Style.slider} ${Style.sliderProgress} w-full h-2 bg-primary rounded-full`}
+      />
+    </div>
   );
 }
 
