cafb457df350ba34b014bac876e3066c96abd0b4
Davide <davide.ceschia@gmail.com>
2023-02-19 02:48:24 +0100

    add tests

--- crates/carbon_domain/src/maven/mod.rs
+++ crates/carbon_domain/src/maven/mod.rs
@@ -18,7 +18,7 @@ pub enum MavenError {
 /// Needs to be in the format of `group:artifact:version`
 /// This is not the full maven specification but should be enough for our use case
 fn is_maven_coordinates(maven_coordinates: &str) -> bool {
-    Regex::new(r#"^[a-zA-Z0-9._-]+:[a-zA-Z0-9._-]+:[0-9]+\.[0-9]+\.[0-9]+$"#)
+    Regex::new(r#"^[a-zA-Z0-9._-]+:[a-zA-Z0-9._-]+:[0-9]+\.[0-9]+(\.[0-9]+)?(-[a-zA-Z0-9._-]+)*(\.[a-zA-Z0-9._-]+)*$"#)
         .expect("failed to compile maven coordinates regex!!!")
         .is_match(maven_coordinates)
 }
@@ -52,5 +52,77 @@ impl MavenCoordinates {
             .join(self.group_id)
             .join(self.artifact_id)
             .join(self.version)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_valid_coordinates() {
+        assert!(is_maven_coordinates("com.example:example:1.0.0"));
+        assert!(is_maven_coordinates("com.example:example:1.0"));
+        assert!(is_maven_coordinates(
+            "com.example:example-something:1.0.final"
+        ));
+        assert!(is_maven_coordinates(
+            "com.example:example-something:1.0.0.Final-beta.1"
+        ));
+        assert!(is_maven_coordinates(
+            "com.example.example:example-example:1.0.0"
+        ));
+        assert!(is_maven_coordinates(
+            "com.example.example:example-example:1.0.0-SNAPSHOT"
+        ));
+        assert!(is_maven_coordinates(
+            "com.example.example:example-example:1.0.0-beta.1"
+        ));
+    }
+
+    #[test]
+    fn test_invalid_coordinates() {
+        assert!(!is_maven_coordinates(""));
+        assert!(!is_maven_coordinates("com.example:example:1"));
+        assert!(!is_maven_coordinates("com.example:example"));
+        assert!(!is_maven_coordinates("com.example:example:1.0.0:extra"));
+    }
+
+    #[test]
+    fn test_parse_coordinates() {
+        let coordinates = "com.example:example:1.0.0".to_string();
+        let parsed_coordinates = parse_maven_coordinates(&coordinates).unwrap();
+        assert_eq!(parsed_coordinates.group_id, "com.example");
+        assert_eq!(parsed_coordinates.artifact_id, "example");
+        assert_eq!(parsed_coordinates.version, "1.0.0");
+
+        let coordinates = "com.example.example:example-example:1.0.0-SNAPSHOT".to_string();
+        let parsed_coordinates = parse_maven_coordinates(&coordinates).unwrap();
+        assert_eq!(parsed_coordinates.group_id, "com.example.example");
+        assert_eq!(parsed_coordinates.artifact_id, "example-example");
+        assert_eq!(parsed_coordinates.version, "1.0.0-SNAPSHOT");
+    }
+
+    #[test]
+    fn test_try_from() {
+        let coordinates = "com.example:example:1.0.0".to_string();
+        let parsed_coordinates = MavenCoordinates::try_from(coordinates).unwrap();
+        assert_eq!(parsed_coordinates.group_id, "com.example");
+        assert_eq!(parsed_coordinates.artifact_id, "example");
+        assert_eq!(parsed_coordinates.version, "1.0.0");
+
+        let coordinates = "".to_string();
+        assert!(MavenCoordinates::try_from(coordinates).is_err());
+
+        let coordinates = "com.example.example:example-example:1.0.0.0:extra".to_string();
+        assert!(MavenCoordinates::try_from(coordinates).is_err());
+    }
+
+    #[test]
+    fn test_into_pathbuf() {
+        let coordinates = "com.example:example:1.0.0".to_string();
+        let parsed_coordinates = MavenCoordinates::try_from(coordinates).unwrap();
+        let path = parsed_coordinates.into_pathbuf();
+        assert_eq!(path, PathBuf::from("com.example/example/1.0.0"));
     }
 }
