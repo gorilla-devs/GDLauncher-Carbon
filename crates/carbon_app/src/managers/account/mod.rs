3147c2505d78cf481c2ac28f86c2be2171788ffb
iluminn <123225099+iluminn@users.noreply.github.com>
2023-02-23 15:01:19 -0800

    FE Error type for account login (#81)

--- crates/carbon_app/src/managers/account/mod.rs
+++ crates/carbon_app/src/managers/account/mod.rs
@@ -15,6 +15,7 @@ use std::mem;
 use thiserror::Error;
 use tokio::sync::RwLock;
 
+pub use self::enroll::EnrollmentError;
 use self::{
     api::DeviceCode,
     enroll::{EnrollmentStatus, EnrollmentTask},
@@ -418,7 +419,7 @@ pub enum FEEnrollmentStatus {
     PollingCode(DeviceCode),
     QueryAccount,
     Complete(Account),
-    Failed(String),
+    Failed(EnrollmentError),
 }
 
 impl FEEnrollmentStatus {
@@ -432,7 +433,7 @@ impl FEEnrollmentStatus {
                 uuid: account.mc.profile.uuid.clone(),
                 type_: AccountType::Microsoft,
             }),
-            EnrollmentStatus::Failed(err) => FEEnrollmentStatus::Failed(format!("{:#?}", err)),
+            EnrollmentStatus::Failed(err) => FEEnrollmentStatus::Failed(err.clone()),
         }
     }
 }
