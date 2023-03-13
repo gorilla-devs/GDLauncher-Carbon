ecf51462dfdbdb3aaf351fc74e2a3038aa976f5d
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2023-03-13 08:31:15 +0100

    navigate to library when authenticated

--- apps/desktop/packages/mainWindow/src/pages/Login/index.tsx
+++ apps/desktop/packages/mainWindow/src/pages/Login/index.tsx
@@ -1,10 +1,9 @@
 import { Dropdown } from "@gd/ui";
-import { createSignal, Switch, Match, onMount } from "solid-js";
+import { createSignal, Switch, Match } from "solid-js";
 import Auth from "./Auth";
 import CodeStep from "./CodeStep";
 import fetchData from "./auth.login.data";
-import { useRouteData } from "@solidjs/router";
-import { useGDNavigate } from "@/managers/NavigationManager";
+import { Navigate, useRouteData } from "@solidjs/router";
 
 export type DeviceCodeObjectType = {
   userCode: string;
@@ -18,64 +17,72 @@ export default function Login() {
     createSignal<DeviceCodeObjectType | null>(null);
 
   const routeData: ReturnType<typeof fetchData> = useRouteData();
-  const navigate = useGDNavigate();
-
-  onMount(() => {
-    if (routeData.activeUuid.data && routeData.accounts?.data?.length! > 0) {
-      navigate("/library");
-    } else setStep(0);
-  });
+  const isAlreadyAuthenticated = () =>
+    routeData.activeUuid.data && routeData.accounts?.data?.length! > 0;
 
   return (
-    <div class="flex justify-center items-center w-full h-screen p-0 bg-img-loginBG.jpg">
-      <div
-        style={{
-          "mix-blend-mode": "hard-light",
-        }}
-        class="absolute left-0 right-0 bg-shade-8 top-0 bottom-0 opacity-80"
-      />
-      <div class="absolute top-0 top-5 z-10 left-1/2 -translate-x-1/2">
-        <Dropdown
-          options={[
-            { label: "english", key: "en" },
-            { label: "italian", key: "it" },
-          ]}
-          value={"asc"}
-          onChange={() => {
-            // getTranslationByLanguage(lang.key).then((translations) => {
-            //   add(lang.key, translations);
-            //   locale(lang.key);
-            // });
-          }}
-          rounded
-        />
-      </div>
-      <div
-        class="flex flex-col items-center text-white relative justify-end rounded-2xl w-120 h-100"
-        style={{
-          background: "rgba(29, 32, 40, 0.8)",
-          "justify-content": step() === 0 ? "flex-end" : "center",
-        }}
-        classList={{
-          "overflow-hidden": step() === 1,
-        }}
-      >
-        <Switch
-          fallback={
-            <Auth setStep={setStep} setDeviceCodeObject={setDeviceCodeObject} />
-          }
-        >
-          <Match when={step() === 0}>
-            <Auth setStep={setStep} setDeviceCodeObject={setDeviceCodeObject} />
-          </Match>
-          <Match when={step() === 1}>
-            <CodeStep
-              deviceCodeObject={deviceCodeObject()}
-              setDeviceCodeObject={setDeviceCodeObject}
+    <Switch>
+      <Match when={isAlreadyAuthenticated()}>
+        <Navigate href={"/library"} />
+      </Match>
+      <Match when={!isAlreadyAuthenticated()}>
+        <div class="flex justify-center items-center w-full h-screen p-0 bg-img-loginBG.jpg">
+          <div
+            style={{
+              "mix-blend-mode": "hard-light",
+            }}
+            class="absolute left-0 right-0 bg-shade-8 top-0 bottom-0 opacity-80"
+          />
+          <div class="absolute top-0 top-5 z-10 left-1/2 -translate-x-1/2">
+            <Dropdown
+              options={[
+                { label: "english", key: "en" },
+                { label: "italian", key: "it" },
+              ]}
+              value={"asc"}
+              onChange={() => {
+                // getTranslationByLanguage(lang.key).then((translations) => {
+                //   add(lang.key, translations);
+                //   locale(lang.key);
+                // });
+              }}
+              rounded
             />
-          </Match>
-        </Switch>
-      </div>
-    </div>
+          </div>
+          <div
+            class="flex flex-col items-center text-white relative justify-end rounded-2xl w-120 h-100"
+            style={{
+              background: "rgba(29, 32, 40, 0.8)",
+              "justify-content": step() === 0 ? "flex-end" : "center",
+            }}
+            classList={{
+              "overflow-hidden": step() === 1,
+            }}
+          >
+            <Switch
+              fallback={
+                <Auth
+                  setStep={setStep}
+                  setDeviceCodeObject={setDeviceCodeObject}
+                />
+              }
+            >
+              <Match when={step() === 0}>
+                <Auth
+                  setStep={setStep}
+                  setDeviceCodeObject={setDeviceCodeObject}
+                />
+              </Match>
+              <Match when={step() === 1}>
+                <CodeStep
+                  deviceCodeObject={deviceCodeObject()}
+                  setDeviceCodeObject={setDeviceCodeObject}
+                />
+              </Match>
+            </Switch>
+          </div>
+        </div>
+      </Match>
+    </Switch>
   );
 }
