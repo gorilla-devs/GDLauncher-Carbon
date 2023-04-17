987d6616a73815d439827d3c7c9c57bb96a73d3e
Davide Ceschia <10165264+killpowa@users.noreply.github.com>
2023-04-18 00:23:06 +0200

    fixed optional iridium (#112)
    * test
    
    * add iridium as submodule
    
    * oops
    
    * fix?
    
    * website removed
    
    * buildWebsite ci removed
    
    * scss file removed
    
    * fix
    
    * fix
    
    ---------
    
    Co-authored-by: Gianmarco Cavallo <cavallogianmarco@gmail.com>

--- packages/ui/src/Select/index.tsx
+++ packages/ui/src/Select/index.tsx
@@ -1,13 +1,11 @@
 import { children } from "solid-js";
-import styles from "./Select.module.scss";
-
 interface Props {
   children: HTMLElement | string;
 }
 
 function Select(props: Props) {
   const c = children(() => props.children);
-  return <div class={styles.paolino}>{c()}</div>;
+  return <div>{c()}</div>;
 }
 
 export { Select };
