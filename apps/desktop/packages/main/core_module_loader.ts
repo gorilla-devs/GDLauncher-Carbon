46a888c31c37929c09d85734d7030efddec72714
Davide <davide.ceschia@gmail.com>
2023-04-17 14:50:30 +0200

    send better errors

--- apps/desktop/packages/main/core_module_loader.ts
+++ apps/desktop/packages/main/core_module_loader.ts
@@ -9,16 +9,23 @@ const binaryName =
 
 type CoreModule = () => Promise<number>;
 
+type Log = {
+  type: "info" | "error";
+  message: string;
+};
+
+type CoreModuleError = {
+  exitCode: number | null;
+  message: string;
+  logs: Log[];
+};
+
 const loadCoreModule: CoreModule = () =>
   new Promise((resolve, reject) => {
     if (isDev) {
       resolve(4650);
       return;
     }
-
-    setTimeout(() => {
-      reject(new Error("Core module failed to start in time"));
-    }, 10000);
 
     const coreModulePath = path.resolve(
       __dirname,
@@ -35,9 +42,14 @@ const loadCoreModule: CoreModule = () =>
         RUST_BACKTRACE: "full",
       },
     });
+    let logs: Log[] = [];
 
     coreModule.stdout.on("data", (data) => {
       let dataString = data.toString();
+      logs.push({
+        type: "info",
+        message: dataString,
+      });
       if (dataString.startsWith("_STATUS_: ")) {
         let port = dataString.split("|")[1];
         resolve(port);
@@ -46,6 +58,10 @@ const loadCoreModule: CoreModule = () =>
     });
 
     coreModule.stderr.on("data", (data) => {
+      logs.push({
+        type: "error",
+        message: data.toString(),
+      });
       console.error(`[CORE] Error: ${data.toString()}`);
     });
 
@@ -53,11 +69,23 @@ const loadCoreModule: CoreModule = () =>
       console.log(`[CORE] Exit with code: ${code}`);
 
       if (code !== 0) {
-        reject(new Error(`Core module exited with code ${code}`));
+        reject({
+          exitCode: code,
+          message: "Core module failed to start",
+          logs,
+        } as CoreModuleError);
       }
 
       resolve(0);
     });
+
+    setTimeout(() => {
+      reject({
+        message: "Core module failed to start",
+        exitCode: null,
+        logs,
+      } as CoreModuleError);
+    }, 10000);
   });
 
 export default loadCoreModule;
