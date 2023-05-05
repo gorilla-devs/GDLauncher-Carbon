2e91b6c28e9afb083e0986d7b4844859c7f08700
Gianmarco Cavallo <cavallogianmarco@gmail.com>
2023-05-05 13:53:24 +0200

    skeleton fix

--- packages/ui/src/Skeleton/index.tsx
+++ packages/ui/src/Skeleton/index.tsx
@@ -4,7 +4,7 @@ const Skeleton = () => {
 
 Skeleton.sidebarInstance = () => {
   return (
-    <div class="flex gap-2">
+    <div class="flex gap-2 py-2 px-4">
       <div class="w-10 h-10 rounded-lg bg-darkSlate-700" />
       <div class="flex flex-col space-between gap-2">
         <div class="w-32 h-4 rounded-md bg-darkSlate-700" />
