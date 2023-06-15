3e6590750054fb22327c6a142657d27804d873c6
Gianmarco <cavallogianmarco@gmail.com>
2023-06-15 17:07:31 +0200

    close log tab in logViewer

--- apps/desktop/packages/mainWindow/src/managers/ModalsManager/modals/LogViewer.tsx
+++ apps/desktop/packages/mainWindow/src/managers/ModalsManager/modals/LogViewer.tsx
@@ -12,6 +12,7 @@ const fetchLogs = async (logId: number) =>
 
 const LogViewer = (props: ModalProps) => {
   const instances = rspc.createQuery(() => ["instance.getInstancesUngrouped"]);
+  const deleteLogsMutation = rspc.createMutation(["instance.deleteLog"]);
 
   const logs = rspc.createQuery(() => ["instance.getLogs"]);
 
@@ -21,7 +22,7 @@ const LogViewer = (props: ModalProps) => {
         <Show when={(logs.data?.length || 0) > 0}>
           <div class="bg-darkSlate-800 max-h-full">
             <Tabs variant="traditional">
-              <div class="flex items-center max-h-full">
+              <div class="flex items-center max-h-full scrollbar-hide">
                 <TabList>
                   <For each={logs.data}>
                     {(log) => {
@@ -32,19 +33,26 @@ const LogViewer = (props: ModalProps) => {
                         <Tab>
                           <div class="w-full flex gap-2 items-center h-10 justify-start">
                             <div class="w-6 rounded-md bg-green h-6" />
-                            <p class="whitespace-nowrap my-2">
-                              {instance?.name}
-                            </p>
+                            <div class="whitespace-nowrap my-2 flex gap-2">
+                              <span>{instance?.name}</span>
+                              <div
+                                class="i-ri:close-fill hover:text-lightGray-50 transition-color ease-in-out duration-100"
+                                onClick={(e) => {
+                                  e.stopPropagation();
+                                  deleteLogsMutation.mutate(log.id);
+                                }}
+                              />
+                            </div>
                           </div>
                         </Tab>
                       );
                     }}
                   </For>
                 </TabList>
-                <div class="flex gap-4 pb-2 px-5">
+                {/* <div class="flex gap-4 pb-2 px-5">
                   <div class="cursor-pointer text-darkSlate-50 i-ri:upload-2-line" />
                   <div class="text-darkSlate-50 cursor-pointer i-ri:file-copy-fill" />
-                </div>
+                </div> */}
               </div>
               <div class="bg-darkSlate-700 overflow-y-auto max-h-130">
                 <For each={logs.data}>
@@ -66,6 +74,7 @@ const LogViewer = (props: ModalProps) => {
 
                     createEffect(() => {
                       if (logs()?.body) {
+                        setLogsObj(instance.id, []);
                         processStream((logs() as any).body);
                       }
                     });
