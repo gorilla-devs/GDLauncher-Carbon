3183ebca4e116a69baefbbe9f1cd2f81d077705b
Davide <davide.ceschia@gmail.com>
2023-06-20 01:37:29 +0200

    dev iridium

--- crates/carbon_app/src/iridium_client.rs
+++ crates/carbon_app/src/iridium_client.rs
@@ -7,6 +7,30 @@ pub fn get_client() -> reqwest_middleware::ClientBuilder {
 #[cfg(not(feature = "production"))]
 #[inline(always)]
 pub fn get_client() -> reqwest_middleware::ClientBuilder {
+    use reqwest::{Request, Response};
+    use reqwest_middleware::{Middleware, Next};
+
+    struct AddHeaderMiddleware;
+
+    #[async_trait::async_trait]
+    impl Middleware for AddHeaderMiddleware {
+        async fn handle(
+            &self,
+            mut req: Request,
+            _extensions: &mut task_local_extensions::Extensions,
+            next: Next<'_>,
+        ) -> reqwest_middleware::Result<Response> {
+            // Add the header to the request.
+            req.headers_mut().insert(
+                "authentication",
+                std::env::var("API_AUTH").unwrap().parse().unwrap(),
+            );
+
+            // Continue with the modified request.
+            next.run(req, _extensions).await
+        }
+    }
+
     let client = reqwest::Client::builder().build().unwrap();
-    reqwest_middleware::ClientBuilder::new(client)
+    reqwest_middleware::ClientBuilder::new(client).with(AddHeaderMiddleware)
 }
