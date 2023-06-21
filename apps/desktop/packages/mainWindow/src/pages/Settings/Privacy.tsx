6944cccbd841d1830b83c389766ed7a0862579e6
Gianmarco <cavallogianmarco@gmail.com>
2023-06-21 23:59:55 +0200

    unused code removed

--- apps/desktop/packages/mainWindow/src/pages/Settings/Privacy.tsx
+++ apps/desktop/packages/mainWindow/src/pages/Settings/Privacy.tsx
@@ -1,35 +1,8 @@
 /* eslint-disable i18next/no-literal-string */
-import { Button, Dropdown, Input, Switch } from "@gd/ui";
-import GDLauncherWideLogo from "/assets/images/gdlauncher_logo.svg";
-import GDLauncherText from "/assets/images/GDLauncher_text.svg";
-import { Trans, useTransContext } from "@gd/i18n";
-import { queryClient, rspc } from "@/utils/rspcClient";
-import SettingsData from "./settings.Privacy.data";
-import { useRouteData } from "@solidjs/router";
-import { createEffect } from "solid-js";
-import { createStore } from "solid-js/store";
-import { FESettings } from "@gd/core_module/bindings";
+import { Button } from "@gd/ui";
+import { Trans } from "@gd/i18n";
 
 const Privacy = () => {
-  const routeData: ReturnType<typeof SettingsData> = useRouteData();
-  const [t, { changeLanguage }] = useTransContext();
-
-  const [settings, setSettings] = createStore<FESettings>(
-    // @ts-ignore
-    routeData?.data?.data || {}
-  );
-
-  const settingsMutation = rspc.createMutation(["settings.setSettings"], {
-    onMutate: (newSettings) => {
-      if (newSettings.language) changeLanguage(newSettings.language as string);
-      queryClient.setQueryData(["settings.getSettings"], newSettings);
-    },
-  });
-
-  createEffect(() => {
-    if (routeData.data.data) setSettings(routeData.data.data);
-  });
-
   return (
     <div class="w-full flex flex-col py-5 box-border bg-darkSlate-800 h-auto px-6">
       <h2 class="m-0 mb-7 text-4">
