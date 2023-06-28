fa2aa210c3b0630bebda2d61bdd57c577e3b8eec
Gianmarco <cavallogianmarco@gmail.com>
2023-06-28 21:35:38 +0200

    wip fix news carousel

--- packages/ui/src/News/index.tsx
+++ packages/ui/src/News/index.tsx
@@ -1,12 +1,4 @@
-import {
-  createEffect,
-  createSignal,
-  For,
-  mergeProps,
-  onCleanup,
-  onMount,
-  Show,
-} from "solid-js";
+import { createSignal, For, mergeProps, onMount, Show } from "solid-js";
 
 type SlideProps = {
   image: string;
@@ -36,7 +28,7 @@ interface CarouselProps {
 }
 
 const News = (props: CarouselProps) => {
-  const [currentImageIndex, setCurrentImageIndex] = createSignal(0);
+  const [currentImageIndex, setCurrentImageIndex] = createSignal(1);
   let interval: any;
 
   const mergedProps = mergeProps(
@@ -45,13 +37,50 @@ const News = (props: CarouselProps) => {
   );
   let slidesRef: HTMLDivElement;
 
+  const moveSlide = () => {
+    slidesRef.style.transform = `translateX(-${currentImageIndex() * 100}%)`;
+  };
+
+  onMount(() => {
+    slidesRef.addEventListener("transitionend", () => {
+      if (currentImageIndex() === 0) {
+        slidesRef.style.transition = `none`;
+        setCurrentImageIndex(props.slides.length - 2);
+        moveSlide();
+      }
+      if (currentImageIndex() === props.slides.length - 1) {
+        slidesRef.style.transition = `none`;
+        setCurrentImageIndex(1);
+        moveSlide();
+      }
+    });
+  });
+
+  const changeSlide = (direction: "right" | "left") => {
+    slidesRef.style.transition = `transform 450ms ease-in-out`;
+    if (direction === "right") {
+      setCurrentImageIndex((prev) => prev + 1);
+    } else {
+      setCurrentImageIndex((prev) => prev - 1);
+    }
+    moveSlide();
+  };
+
   const Slider = (props: SliderProps) => {
+    const firstSlide = () => props.slides[0];
+    const lastSlide = () => props.slides[props.slides.length - 2];
+    const copiedSlides = () => [lastSlide(), ...props.slides, firstSlide()];
+
+    onMount(() => {
+      moveSlide();
+    });
+
     return (
-      <div ref={slidesRef} id="slider" class="flex h-80">
-        <For each={props.slides}>
+      <div ref={slidesRef} class="flex h-80">
+        <For each={copiedSlides()}>
           {(slide) => (
             <div
-              class="absolute inset-0 transition-all transform min-h-80 w-full flex justify-center items-center hidden box-border bg-no-repeat	bg-center bg-cover cursor-pointer"
+              class="flex-grow flex-shrink-0 inset-0 transform min-h-80 w-full flex justify-center items-center hidden box-border bg-no-repeat	bg-center bg-cover cursor-pointer"
               style={{
                 "background-image": `url('${slide.image}')`,
               }}
@@ -75,95 +104,6 @@ const News = (props: CarouselProps) => {
     );
   };
 
-  onMount(() => {
-    const slides = slidesRef?.children as HTMLCollection;
-
-    const leftSlide = () =>
-      currentImageIndex() === 0
-        ? slides[slides.length - 1]
-        : slides[currentImageIndex() - 1];
-    const middleSlide = () => slides[currentImageIndex()];
-    const rightSlide = () =>
-      currentImageIndex() === slides.length - 1
-        ? slides[0]
-        : slides[currentImageIndex() + 1];
-
-    leftSlide().classList.add("-translate-x-full", "z-10");
-
-    middleSlide().classList.add("translate-x-0", "z-20");
-
-    rightSlide().classList.add("translate-x-full", "z-10");
-  });
-
-  createEffect(() => {
-    interval = setInterval(() => {
-      if (!props.disableAutoRotation) {
-        changeSlide(mergedProps.rtl ? "right" : "left");
-      }
-    }, props.speed || 5000);
-  });
-
-  onCleanup(() => clearInterval(interval));
-
-  const slideInto = (slides: HTMLCollection, position: number) => {
-    const leftSlide = () =>
-      position === 0 ? slides[slides.length - 1] : slides[position - 1];
-    const middleSlide = () => slides[position];
-    const rightSlide = () =>
-      position === slides.length - 1 ? slides[0] : slides[position + 1];
-
-    leftSlide().classList.remove(
-      "-translate-x-full",
-      "translate-x-full",
-      "translate-x-0",
-      "hidden",
-      "z-20"
-    );
-    leftSlide().classList.add("-translate-x-full", "z-10");
-    middleSlide().classList.remove(
-      "-translate-x-full",
-      "translate-x-full",
-      "translate-x-0",
-      "hidden",
-      "z-10"
-    );
-    middleSlide().classList.add("translate-x-0", "z-20");
-    rightSlide().classList.remove(
-      "-translate-x-full",
-      "translate-x-full",
-      "translate-x-0",
-      "hidden",
-      "z-20"
-    );
-    rightSlide().classList.add("translate-x-full", "z-10");
-    setCurrentImageIndex(position);
-  };
-
-  const changeSlide = (direction: "right" | "left") => {
-    clearInterval(interval);
-    interval = setInterval(() => {
-      changeSlide("right");
-    }, props.speed || 5000);
-
-    const slides = slidesRef?.children as HTMLCollection;
-
-    const isRight = direction === "right";
-
-    const isNotLastElement = isRight
-      ? currentImageIndex() < props.slides.length - 1
-      : currentImageIndex() > 0;
-
-    if (!slides) return;
-    if (isNotLastElement) {
-      slideInto(
-        slides,
-        isRight ? currentImageIndex() + 1 : currentImageIndex() - 1
-      );
-    } else {
-      slideInto(slides, isRight ? 0 : props.slides.length - 1);
-    }
-  };
-
   return (
     <div class="h-80 bg-darkSlate-900 rounded-lg relative overflow-hidden relative">
       <Show when={mergedProps.showArrows}>
@@ -182,16 +122,17 @@ const News = (props: CarouselProps) => {
       </Show>
       <Show when={mergedProps.showIndicators}>
         <div class="flex justify-between items-center gap-2 z-50 absolute bottom-4 left-1/2 -translate-x-1/2">
-          <For each={props.slides}>
+          <For each={props.slides.slice(1, -1)}>
             {(_, i) => (
               <div
                 class={`w-2 h-2 bg-white rounded-full cursor-pointer ${
-                  currentImageIndex() === i() ? "opacity-100" : "opacity-30"
+                  currentImageIndex() === i() + 1 ? "opacity-100" : "opacity-30"
                 }`}
                 onClick={() => {
-                  const slides = slidesRef?.children as HTMLCollection;
+                  slidesRef.style.transition = `transform 450ms ease-in-out`;
 
-                  if (slides) slideInto(slides, i());
+                  slidesRef.style.transform = `translateX(-${i() * 100}%)`;
+                  setCurrentImageIndex(i() + 1);
                 }}
               />
             )}
