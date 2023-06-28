4ffe7fec8f86397a607d7d4d065a3761d6f35541
Gianmarco <cavallogianmarco@gmail.com>
2023-06-28 23:19:16 +0200

    carousel refactor

--- packages/ui/src/News/index.tsx
+++ packages/ui/src/News/index.tsx
@@ -1,4 +1,12 @@
-import { createSignal, For, mergeProps, onMount, Show } from "solid-js";
+import {
+  createEffect,
+  createSignal,
+  For,
+  mergeProps,
+  onCleanup,
+  onMount,
+  Show,
+} from "solid-js";
 
 type SlideProps = {
   image: string;
@@ -10,7 +18,6 @@ type SlideProps = {
 type SliderProps = {
   currentImageIndex: number;
   slides: SlideProps[];
-  disableRedirect?: boolean;
   alignment?: string;
   onClick?: any;
   onSlideClick?: (_news: SlideProps) => void;
@@ -23,13 +30,13 @@ interface CarouselProps {
   disableAutoRotation?: boolean;
   showArrows?: boolean;
   showIndicators?: boolean;
-  disableRedirect?: boolean;
   onClick?: (_news: SlideProps) => void;
 }
 
 const News = (props: CarouselProps) => {
   const [currentImageIndex, setCurrentImageIndex] = createSignal(1);
-  let interval: any;
+  const [isMoving, setIsMoving] = createSignal(false);
+  let interval: ReturnType<typeof setInterval>;
 
   const mergedProps = mergeProps(
     { showIndicators: true, showArrows: true, rtl: true },
@@ -41,22 +48,34 @@ const News = (props: CarouselProps) => {
     slidesRef.style.transform = `translateX(-${currentImageIndex() * 100}%)`;
   };
 
+  const firstSlide = () => props.slides[0];
+  const lastSlide = () => props.slides[props.slides.length - 1];
+  const copiedSlides = () => [lastSlide(), ...props.slides, firstSlide()];
+
+  const handleTransitionEnd = () => {
+    setIsMoving(false);
+    if (currentImageIndex() === 0) {
+      slidesRef.style.transition = `none`;
+      setCurrentImageIndex(copiedSlides().length - 2);
+      moveSlide();
+    }
+    if (currentImageIndex() === copiedSlides().length - 1) {
+      slidesRef.style.transition = `none`;
+      setCurrentImageIndex(1);
+      moveSlide();
+    }
+  };
+
   onMount(() => {
-    slidesRef.addEventListener("transitionend", () => {
-      if (currentImageIndex() === 0) {
-        slidesRef.style.transition = `none`;
-        setCurrentImageIndex(props.slides.length - 2);
-        moveSlide();
-      }
-      if (currentImageIndex() === props.slides.length - 1) {
-        slidesRef.style.transition = `none`;
-        setCurrentImageIndex(1);
-        moveSlide();
-      }
-    });
+    slidesRef.addEventListener("transitionend", handleTransitionEnd);
+  });
+
+  onCleanup(() => {
+    slidesRef.removeEventListener("transitionend", handleTransitionEnd);
   });
 
   const changeSlide = (direction: "right" | "left") => {
+    setIsMoving(true);
     slidesRef.style.transition = `transform 450ms ease-in-out`;
     if (direction === "right") {
       setCurrentImageIndex((prev) => prev + 1);
@@ -66,11 +85,17 @@ const News = (props: CarouselProps) => {
     moveSlide();
   };
 
-  const Slider = (props: SliderProps) => {
-    const firstSlide = () => props.slides[0];
-    const lastSlide = () => props.slides[props.slides.length - 2];
-    const copiedSlides = () => [lastSlide(), ...props.slides, firstSlide()];
+  createEffect(() => {
+    interval = setInterval(() => {
+      if (!props.disableAutoRotation) {
+        changeSlide(mergedProps.rtl ? "right" : "left");
+      }
+    }, props.speed || 5000);
+  });
 
+  onCleanup(() => clearInterval(interval));
+
+  const Slider = (props: SliderProps) => {
     onMount(() => {
       moveSlide();
     });
@@ -80,21 +105,26 @@ const News = (props: CarouselProps) => {
         <For each={copiedSlides()}>
           {(slide) => (
             <div
-              class="flex-grow flex-shrink-0 inset-0 transform min-h-80 w-full flex justify-center items-center hidden box-border bg-no-repeat	bg-center bg-cover cursor-pointer"
+              class="flex-grow flex-shrink-0 inset-0 transform min-h-80 w-full flex justify-center items-center hidden box-border bg-no-repeat bg-center bg-cover"
               style={{
                 "background-image": `url('${slide.image}')`,
               }}
-              onClick={() => props.onSlideClick?.(slide)}
             >
               <div
                 class="absolute bottom-0 left-0 right-0 top-0"
                 style={{
                   background:
-                    "linear-gradient(180deg, rgba(29, 32, 40, 0) 0%, #1D2028 100%)",
+                    "linear-gradient(180deg, rgba(29, 32, 40, 0) 0%, var(--darkSlate-800) 100%)",
                 }}
               />
               <div class="absolute bottom-10 left-5 flex flex-col select-none">
-                <h2 class="mb-0">{slide.title}</h2>
+                <div
+                  class="group flex gap-2 items-center cursor-pointer"
+                  onClick={() => props.onSlideClick?.(slide)}
+                >
+                  <h2 class="m-0 group-hover:underline">{slide.title}</h2>
+                  <div class="peer i-ri:external-link-line" />
+                </div>
                 <p class="mt-2 text-darkSlate-50">{slide.description}</p>
               </div>
             </div>
@@ -109,20 +139,26 @@ const News = (props: CarouselProps) => {
       <Show when={mergedProps.showArrows}>
         <div
           class="h-7 w-7 bg-darkSlate-800 rounded-full absolute left-5 top-1/2 -translate-y-1/2 flex justify-center items-center cursor-pointer z-40 "
-          onClick={() => changeSlide("left")}
+          onClick={() => {
+            if (isMoving()) return;
+            changeSlide("left");
+          }}
         >
           <div class="i-ri:arrow-drop-left-line text-3xl text-white" />
         </div>
         <div
           class="h-7 w-7 bg-darkSlate-800 rounded-full absolute right-5 top-1/2 -translate-y-1/2 flex justify-center items-center cursor-pointer z-40"
-          onClick={() => changeSlide("right")}
+          onClick={() => {
+            if (isMoving()) return;
+            changeSlide("right");
+          }}
         >
           <div class="i-ri:arrow-drop-right-line text-3xl text-white" />
         </div>
       </Show>
       <Show when={mergedProps.showIndicators}>
         <div class="flex justify-between items-center gap-2 z-50 absolute bottom-4 left-1/2 -translate-x-1/2">
-          <For each={props.slides.slice(1, -1)}>
+          <For each={copiedSlides().slice(1, -1)}>
             {(_, i) => (
               <div
                 class={`w-2 h-2 bg-white rounded-full cursor-pointer ${
@@ -130,7 +166,6 @@ const News = (props: CarouselProps) => {
                 }`}
                 onClick={() => {
                   slidesRef.style.transition = `transform 450ms ease-in-out`;
-
                   slidesRef.style.transform = `translateX(-${i() * 100}%)`;
                   setCurrentImageIndex(i() + 1);
                 }}
