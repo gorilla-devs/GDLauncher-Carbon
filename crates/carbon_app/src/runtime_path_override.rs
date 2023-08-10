df4f186d2ec8f8ba6b309df98d65ed446a7cb6c1
Davide <davide.ceschia@gmail.com>
2023-08-11 01:07:42 +0200

    avoid lint

--- crates/carbon_app/src/runtime_path_override.rs
+++ crates/carbon_app/src/runtime_path_override.rs
@@ -1,7 +1,7 @@
 use std::{env, path::PathBuf};
 
 pub(crate) async fn get_runtime_path_override() -> PathBuf {
-    let mut path: Option<PathBuf> = None;
+    let mut path: Option<PathBuf>;
     #[cfg(debug_assertions)]
     {
         path = Some(PathBuf::from(env!("RUNTIME_PATH")));
