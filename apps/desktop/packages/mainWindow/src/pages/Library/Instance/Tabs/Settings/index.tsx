2cabc68eebfc68529146d5fade46cd0630f986a8
Davide <davide.ceschia@gmail.com>
2023-09-20 19:10:18 +0200

    improve instance settings

--- apps/desktop/packages/mainWindow/src/pages/Library/Instance/Tabs/Settings/index.tsx
+++ apps/desktop/packages/mainWindow/src/pages/Library/Instance/Tabs/Settings/index.tsx
@@ -5,7 +5,8 @@ import { Trans } from "@gd/i18n";
 import { Button, Input, Slider, Switch } from "@gd/ui";
 import { useParams, useRouteData } from "@solidjs/router";
 import fetchData from "../../instance.data";
-import { Show } from "solid-js";
+import { Show, Suspense } from "solid-js";
+import { InstanceDetails } from "@gd/core_module/bindings";
 
 const Settings = () => {
   const params = useParams();
@@ -23,47 +24,25 @@ const Settings = () => {
   const mbTotalRAM = () => Number(routeData.totalRam.data) / 1024 / 1024;
 
   return (
-    <div class="pt-10 divide-y divide-darkSlate-600">
-      <div class="mb-6">
-        <div class="w-full flex justify-between items-center mb-4">
-          <h5 class="m-0">
-            <Trans key="java.java_memory_title" />
-          </h5>
-          <Switch
-            checked={!!routeData?.instanceDetails?.data?.memory}
-            onChange={(e) => {
-              updateInstanceMutation.mutate({
-                memory: {
-                  Set: e.target.checked
-                    ? {
-                        max_mb: Math.round(mbTotalRAM() / 2),
-                        min_mb: Math.round(mbTotalRAM() / 2),
-                      }
-                    : null,
-                },
-                extra_java_args: null,
-                global_java_args: null,
-                modloader: null,
-                name: null,
-                notes: null,
-                use_loaded_icon: null,
-                version: null,
-                instance: parseInt(params.id, 10),
-              });
-            }}
-          />
-        </div>
-        <Show when={routeData?.instanceDetails?.data?.memory !== null}>
-          <div class="flex justify-center px-2">
-            <Slider
-              min={0}
-              max={mbTotalRAM()}
-              steps={1000}
-              value={routeData?.instanceDetails.data?.memory?.max_mb}
-              marks={generateSequence(1024, mbTotalRAM())}
-              onChange={(val) =>
+    <Suspense fallback={null}>
+      <div class="pt-10 divide-y divide-darkSlate-600">
+        <div class="mb-6">
+          <div class="w-full flex justify-between items-center mb-4">
+            <h5 class="m-0">
+              <Trans key="java.java_memory_title" />
+            </h5>
+            <Switch
+              checked={!!routeData?.instanceDetails?.data?.memory}
+              onChange={(e) => {
                 updateInstanceMutation.mutate({
-                  memory: { Set: { max_mb: val, min_mb: val } },
+                  memory: {
+                    Set: e.target.checked
+                      ? {
+                          max_mb: Math.round(mbTotalRAM() / 2),
+                          min_mb: Math.round(mbTotalRAM() / 2),
+                        }
+                      : null,
+                  },
                   extra_java_args: null,
                   global_java_args: null,
                   modloader: null,
@@ -72,44 +51,78 @@ const Settings = () => {
                   use_loaded_icon: null,
                   version: null,
                   instance: parseInt(params.id, 10),
-                })
-              }
+                });
+              }}
             />
           </div>
-        </Show>
-      </div>
+          <Show when={routeData?.instanceDetails?.data?.memory !== null}>
+            <div class="flex justify-center px-2">
+              <Slider
+                min={0}
+                max={mbTotalRAM()}
+                steps={1000}
+                value={routeData?.instanceDetails.data?.memory?.max_mb}
+                marks={generateSequence(2048, mbTotalRAM())}
+                onChange={(val) => {
+                  if (
+                    !val ||
+                    routeData?.instanceDetails.data?.memory?.max_mb === val
+                  ) {
+                    return;
+                  }
+                  queryClient.setQueryData(
+                    ["instance.getInstanceDetails"],
+                    (oldData: InstanceDetails | undefined) => {
+                      if (!oldData) return;
+                      oldData.memory = {
+                        max_mb: val,
+                        min_mb: val,
+                      };
+                      return oldData;
+                    }
+                  );
+                }}
+                OnRelease={(val) => {
+                  if (
+                    !val ||
+                    routeData?.instanceDetails.data?.memory?.max_mb === val
+                  ) {
+                    return;
+                  }
 
-      <div class="mb-6">
-        <div class="w-full flex justify-between items-center mb-4">
-          <h5 class="m-0">
-            <Trans key="java.java_arguments_title" />
-          </h5>
-          <Switch
-            checked={!!routeData?.instanceDetails?.data?.extra_java_args}
-            onChange={() => {
-              updateInstanceMutation.mutate({
-                memory: null,
-                extra_java_args: { Set: " " },
-                global_java_args: null,
-                modloader: null,
-                name: null,
-                notes: null,
-                use_loaded_icon: null,
-                version: null,
-                instance: parseInt(params.id, 10),
-              });
-            }}
-          />
+                  updateInstanceMutation.mutate({
+                    memory: { Set: { max_mb: val, min_mb: val } },
+                    extra_java_args: null,
+                    global_java_args: null,
+                    modloader: null,
+                    name: null,
+                    notes: null,
+                    use_loaded_icon: null,
+                    version: null,
+                    instance: parseInt(params.id, 10),
+                  });
+                }}
+              />
+            </div>
+          </Show>
         </div>
-        <Show when={!!routeData?.instanceDetails?.data?.extra_java_args}>
-          <div class="flex w-full gap-4 items-center">
-            <Input
-              class="w-full"
-              value={routeData?.instanceDetails?.data?.extra_java_args || ""}
-              onInput={(e) => {
+
+        <div class="mb-6">
+          <div class="w-full flex justify-between items-center mb-4">
+            <h5 class="m-0">
+              <Trans key="java.java_arguments_title" />
+            </h5>
+            <Switch
+              checked={
+                routeData?.instanceDetails?.data?.extra_java_args !== null &&
+                routeData?.instanceDetails?.data?.extra_java_args !== undefined
+              }
+              onChange={(e) => {
+                const checked = e.target.checked;
+
                 updateInstanceMutation.mutate({
                   memory: null,
-                  extra_java_args: { Set: e.target.value },
+                  extra_java_args: { Set: checked ? "" : null },
                   global_java_args: null,
                   modloader: null,
                   name: null,
@@ -120,30 +133,72 @@ const Settings = () => {
                 });
               }}
             />
-            <Button
-              rounded={false}
-              type="secondary"
-              class="h-10"
-              onClick={() => {
-                updateInstanceMutation.mutate({
-                  memory: null,
-                  extra_java_args: { Set: "" },
-                  global_java_args: null,
-                  modloader: null,
-                  name: null,
-                  notes: null,
-                  use_loaded_icon: null,
-                  version: null,
-                  instance: parseInt(params.id, 10),
-                });
-              }}
-            >
-              <Trans key="java.reset_java_args" />
-            </Button>
           </div>
-        </Show>
+          <Show
+            when={
+              routeData?.instanceDetails?.data?.extra_java_args !== null &&
+              routeData?.instanceDetails?.data?.extra_java_args !== undefined
+            }
+          >
+            <div class="flex w-full gap-4 items-center">
+              <Input
+                class="w-full"
+                value={routeData?.instanceDetails?.data?.extra_java_args || ""}
+                onInput={(e) => {
+                  let value = e.target.value;
+                  if (
+                    routeData?.instanceDetails?.data?.extra_java_args === value
+                  ) {
+                    return;
+                  }
+                  queryClient.setQueryData(
+                    ["instance.getInstanceDetails"],
+                    (oldData: InstanceDetails | undefined) => {
+                      if (!oldData || oldData.extra_java_args === value) return;
+                      oldData.extra_java_args = value;
+                      return oldData;
+                    }
+                  );
+                }}
+                onblur={(e) => {
+                  updateInstanceMutation.mutate({
+                    memory: null,
+                    extra_java_args: { Set: e.target.value },
+                    global_java_args: null,
+                    modloader: null,
+                    name: null,
+                    notes: null,
+                    use_loaded_icon: null,
+                    version: null,
+                    instance: parseInt(params.id, 10),
+                  });
+                }}
+              />
+              <Button
+                rounded={false}
+                type="secondary"
+                class="h-10"
+                onClick={() => {
+                  updateInstanceMutation.mutate({
+                    memory: null,
+                    extra_java_args: { Set: "" },
+                    global_java_args: null,
+                    modloader: null,
+                    name: null,
+                    notes: null,
+                    use_loaded_icon: null,
+                    version: null,
+                    instance: parseInt(params.id, 10),
+                  });
+                }}
+              >
+                <Trans key="java.reset_java_args" />
+              </Button>
+            </div>
+          </Show>
+        </div>
       </div>
-    </div>
+    </Suspense>
   );
 };
 
