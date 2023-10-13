54b7b96e7321159a6d5da5c5a5c580112314de1c
Davide <davide.ceschia@gmail.com>
2023-10-13 18:47:39 +0200

    refactor how runtime path works

--- apps/desktop/packages/main/runtimePath.ts
+++ apps/desktop/packages/main/runtimePath.ts
@@ -3,25 +3,21 @@ import fss from "fs";
 import path from "path";
 import fs from "fs/promises";
 import fse from "fs-extra";
-import os from "os";
 import coreModule from "./coreModule";
 
-export const RUNTIME_PATH_DIR_NAME = "gdlauncher_carbon";
 export const RUNTIME_PATH_OVERRIDE_NAME = "runtime_path_override";
+const RUNTIME_PATH_DEFAULT_NAME = "data";
 
-let INITIAL_RUNTIME_PATH_OVERRIDE: string | null = null;
+export let CURRENT_RUNTIME_PATH: string | null = null;
 
 export function initRTPath(override: string | null | undefined) {
   if (override) {
-    INITIAL_RUNTIME_PATH_OVERRIDE = override;
-    setCurrentRTPath(override);
+    CURRENT_RUNTIME_PATH = override;
     return;
   }
 
-  const initialRTPath = getInitialRTPath();
-
   const runtimeOverridePath = path.join(
-    initialRTPath,
+    app.getPath("userData"),
     RUNTIME_PATH_OVERRIDE_NAME
   );
 
@@ -34,74 +30,35 @@ export function initRTPath(override: string | null | undefined) {
     // ignore
   }
 
-  setCurrentRTPath(file_override || initialRTPath);
-}
-
-export function getInitialRTPath() {
-  if (import.meta.env.RUNTIME_PATH) {
-    return import.meta.env.RUNTIME_PATH;
-  } else if (INITIAL_RUNTIME_PATH_OVERRIDE) {
-    return INITIAL_RUNTIME_PATH_OVERRIDE;
-  }
-
-  let runtimePath = null;
-
-  if (os.platform() !== "linux") {
-    runtimePath = app.getPath("appData");
-  } else {
-    // monkey patch linux since it defaults to .config instead of .local/share
-    const xdgDataHome = process.env.XDG_DATA_HOME;
-    if (xdgDataHome) {
-      runtimePath = xdgDataHome;
-    } else {
-      const homeDir = os.homedir();
-      runtimePath = path.join(homeDir, ".local/share");
-    }
-  }
-
-  return path.join(runtimePath, RUNTIME_PATH_DIR_NAME);
-}
-
-export function getCurrentRTPath() {
-  return app.getPath("userData");
-}
-
-export function setCurrentRTPath(newPath: string) {
-  // userData is where electron stores all of its data
-  app.setPath("userData", newPath);
+  CURRENT_RUNTIME_PATH =
+    file_override ||
+    path.join(app.getPath("userData"), RUNTIME_PATH_DEFAULT_NAME);
 }
 
 ipcMain.handle("getInitialRuntimePath", async () => {
-  return getInitialRTPath();
+  return path.join(app.getPath("userData"), RUNTIME_PATH_DEFAULT_NAME);
 });
 
 ipcMain.handle("getRuntimePath", async () => {
-  return getCurrentRTPath();
+  return CURRENT_RUNTIME_PATH;
 });
 
-ipcMain.handle("changeRuntimePath", async (_, _newPath: string | null) => {
-  const initialRTPath = getInitialRTPath();
-  const newPath = _newPath || initialRTPath;
-
-  const currentRTPath = getCurrentRTPath();
-
-  if (newPath === currentRTPath) {
+ipcMain.handle("changeRuntimePath", async (_, newPath: string) => {
+  if (newPath === CURRENT_RUNTIME_PATH) {
     return;
   }
 
   const runtimeOverridePath = path.join(
-    initialRTPath,
+    app.getPath("userData"),
     RUNTIME_PATH_OVERRIDE_NAME
   );
 
   await fs.mkdir(newPath, { recursive: true });
 
-  await fse.copy(currentRTPath, newPath, {
+  // TODO: Copy with progress
+  await fse.copy(CURRENT_RUNTIME_PATH!, newPath, {
     overwrite: true,
-    filter: (src) => {
-      return !src.includes(RUNTIME_PATH_OVERRIDE_NAME);
-    },
-    errorOnExist: false
+    errorOnExist: true
   });
 
   await fs.writeFile(runtimeOverridePath, newPath);
@@ -113,13 +70,14 @@ ipcMain.handle("changeRuntimePath", async (_, _newPath: string | null) => {
     // No op
   }
 
-  await fse.remove(path.join(currentRTPath, "data"));
+  await fse.remove(CURRENT_RUNTIME_PATH!);
 
+  // TODO: with a bit of work we can change the RTPath without actually restarting the app
   app.relaunch();
 });
 
 ipcMain.handle("validateRuntimePath", async (_, newPath: string | null) => {
-  if (!newPath || newPath === getCurrentRTPath()) {
+  if (!newPath || newPath === CURRENT_RUNTIME_PATH) {
     return false;
   }
 
