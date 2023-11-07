00c0dde7ccfe2c1591f42685fb0d41dca4f70ff8
aymanesarrar <aymensarrar1@gmail.com>
2023-11-07 20:44:23 +0100

    chore: change primary and glow button colors on disabled

--- packages/ui/src/Button/index.tsx
+++ packages/ui/src/Button/index.tsx
@@ -82,11 +82,11 @@ const getVariant = (
   const variants = {
     primary: {
       ...commonStyle,
-      [`${!isDisabled ? `bg-${variant}-500` : "bg-[#404759]"}`]: true,
-      [`${!isDisabled ? `hover:bg-${variant}-300` : `hover:bg-[#495164]`}`]:
+      [`${!isDisabled ? `bg-${variant}-500` : "bg-[#1D2028]"}`]: true,
+      [`${!isDisabled ? `hover:bg-${variant}-300` : `hover:bg-[#535c70]`}`]:
         true,
       // "filter brightness-75": isDisabled,
-      "text-[#8A8B8F]": isDisabled,
+      "text-[#404759]": isDisabled,
       "border-0": true,
     },
     secondary: {
@@ -114,8 +114,8 @@ const getVariant = (
       ...commonStyle,
       [`bg-${variant}-500`]: !isDisabled,
       [`drop-shadow-[0_0px_12px_var(--${variant}-500)]`]: !isDisabled,
-      "bg-darkSlate-500": isDisabled,
-      "text-darkSlate-50": isDisabled,
+      "bg-[#404759]": isDisabled,
+      "text-[#8A8B8F]": isDisabled,
       "border-0": true,
     },
     transparent: {
