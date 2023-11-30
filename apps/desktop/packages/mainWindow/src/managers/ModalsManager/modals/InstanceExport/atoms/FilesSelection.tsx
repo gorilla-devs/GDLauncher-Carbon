95b73cfd6f7367522b09f7995e4c02b66d17e2fa
aymanesarrar <aymensarrar1@gmail.com>
2023-11-30 20:24:20 +0100

    chore: using explore to get all the files inside every directory

--- apps/desktop/packages/mainWindow/src/managers/ModalsManager/modals/InstanceExport/atoms/FilesSelection.tsx
+++ apps/desktop/packages/mainWindow/src/managers/ModalsManager/modals/InstanceExport/atoms/FilesSelection.tsx
@@ -5,19 +5,46 @@ import SingleCheckBox from "../../OnBoarding/SingleCheckBox";
 import { rspc } from "@/utils/rspcClient";
 import { payload } from "..";
 import { instanceId } from "@/utils/browser";
+import { ExploreEntry } from "@gd/core_module/bindings";
 
 const FilesSelection = () => {
   const [t] = useTransContext();
+  const [filesToSelect, setFilesToSelect] = createSignal<ExploreEntry[]>([]);
 
-  const explore = rspc.createQuery(() => [
-    "instance.explore",
-    {
-      instance_id: instanceId() as number,
-      path: []
-    }
-  ]);
+  createEffect(() => {
+    const exploreRecursively = (path: string[] = []) => {
+      const explore = rspc.createQuery(() => [
+        "instance.explore",
+        {
+          instance_id: instanceId() as number,
+          path: path
+        }
+      ]);
 
-  console.log(explore.data);
+      const data = explore.data;
+      const files = data || [];
+
+      for (const file of files) {
+        console.log(file);
+        if (file.type === "Directory") {
+          exploreRecursively([...path, file.name]);
+        } else {
+          setFilesToSelect([...filesToSelect(), file]);
+        }
+      }
+    };
+    //exploreRecursively();
+    // const explore = rspc.createQuery(() => [
+    //   "instance.explore",
+    //   {
+    //     instance_id: instanceId() as number,
+    //     path: []
+    //   }
+    // ]);
+    // console.log(explore.data);
+    // console.log(filesToSelect());
+  });
+
   return (
     <div class="w-full flex flex-col gap-2 pt-2">
       <span>{t("instance.select_files_text")}</span>
