7c97624923c6c50128b10908b1b803a36b5c2b16
aymanesarrar <aymensarrar1@gmail.com>
2023-12-01 01:45:26 +0100

    chore: making a recursive function to go into all directories and sub directories

--- apps/desktop/packages/mainWindow/src/managers/ModalsManager/modals/InstanceExport/atoms/FilesSelection.tsx
+++ apps/desktop/packages/mainWindow/src/managers/ModalsManager/modals/InstanceExport/atoms/FilesSelection.tsx
@@ -6,13 +6,46 @@ import { rspc } from "@/utils/rspcClient";
 import { payload } from "..";
 import { instanceId } from "@/utils/browser";
 import { ExploreEntry } from "@gd/core_module/bindings";
+import { isWithinInterval } from "date-fns";
 
 const FilesSelection = () => {
   const [t] = useTransContext();
   const [filesToSelect, setFilesToSelect] = createSignal<ExploreEntry[]>([]);
+  const [files, setFiles] = createSignal<never[]>([]);
 
-  createEffect(() => {
-    const exploreRecursively = (path: string[] = []) => {
+  if (typeof instanceId() === "number") {
+    console.log(instanceId());
+    // const explore = (path: string[]) => {
+    //   return rspc.createQuery(() => [
+    //     "instance.explore",
+    //     {
+    //       instance_id: instanceId() as number,
+    //       path: path
+    //     }
+    //   ]);
+    // };
+
+    // const exploreDirectories = (path: string[]) => {
+    //   const root = explore(path);
+
+    //   const toBeExplored = root.data ?? [];
+    //   console.log("toBeExplored: ", toBeExplored, toBeExplored.length);
+
+    //   while (toBeExplored.length !== 0) {
+    //     const current = toBeExplored.pop();
+    //     if (!current) continue;
+    //     if (current.type === "Directory") {
+    //       console.log([...path, current.name]);
+    //       const sub = explore([...path, current.name]);
+    //       const subFiles = sub.data ?? [];
+    //       toBeExplored.push(...subFiles);
+    //     } else {
+    //       setFilesToSelect([...filesToSelect(), current]);
+    //     }
+    //   }
+    // };
+
+    const exploreRecursively = (path: string[]) => {
       const explore = rspc.createQuery(() => [
         "instance.explore",
         {
@@ -21,19 +54,22 @@ const FilesSelection = () => {
         }
       ]);
 
-      const data = explore.data;
-      const files = data || [];
+      const files = explore.data ?? [];
 
       for (const file of files) {
-        console.log(file);
+        // console.log("file: ", file.name);
+        // console.log("type: ", file.type);
+        // console.log("path: ", [...path, file.name].join("/"));
         if (file.type === "Directory") {
+          console.log([...path, file.name]);
           exploreRecursively([...path, file.name]);
         } else {
           setFilesToSelect([...filesToSelect(), file]);
         }
       }
     };
-    //exploreRecursively();
+    exploreRecursively([]);
+
     // const explore = rspc.createQuery(() => [
     //   "instance.explore",
     //   {
@@ -43,20 +79,24 @@ const FilesSelection = () => {
     // ]);
     // console.log(explore.data);
     // console.log(filesToSelect());
-  });
+  }
 
   return (
     <div class="w-full flex flex-col gap-2 pt-2">
       <span>{t("instance.select_files_text")}</span>
       <div class="w-full rounded-md bg-darkSlate-900 h-44 overflow-y-scroll">
-        {/* <For each={mockData}>
+        <For each={filesToSelect()}>
           {(item) => (
             <div class="flex flex-row justify-between items-center h-10 px-2">
-              <SingleCheckBox title={item.name} setList={setFiles} />
-              <span>{item.size}</span>
+              <SingleCheckBox
+                items={files()}
+                title={item.name}
+                setList={setFiles}
+              />
+              <span>{item.type["File"].size} kb</span>
             </div>
           )}
-        </For> */}
+        </For>
       </div>
     </div>
   );
