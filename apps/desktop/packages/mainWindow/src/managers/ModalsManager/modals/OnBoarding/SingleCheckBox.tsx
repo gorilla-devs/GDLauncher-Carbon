c5d68f263b0ada470646ff3b81905107eb7f0c68
aymanesarrar <aymensarrar1@gmail.com>
2023-12-10 22:50:58 +0100

    fix: indeterminate state on add import

--- apps/desktop/packages/mainWindow/src/managers/ModalsManager/modals/OnBoarding/SingleCheckBox.tsx
+++ apps/desktop/packages/mainWindow/src/managers/ModalsManager/modals/OnBoarding/SingleCheckBox.tsx
@@ -7,25 +7,20 @@ interface Props {
   setList?: Setter<never[]>;
   setInstance?: (_instance: string | undefined) => void;
   isSingleInstance?: boolean;
-  indeterminate?: boolean;
-  isNested?: boolean;
 }
 
 const SingleCheckBox = (props: Props) => {
   const handleChange = () => {
-    if (!props.isNested) {
-      if (instances().some((instance) => instance === props.title)) {
-        if (props.setList) {
-          props.setList((prev: any) =>
-            prev.filter((e: any) => e !== props.title)
-          );
-        }
-      } else {
-        if (props.setList) {
-          props.setList((prev: any) => [...prev, props.title] as never);
-        }
+    if (instances().some((instance) => instance === props.title)) {
+      if (props.setList) {
+        props.setList((prev: any) =>
+          prev.filter((e: any) => e !== props.title)
+        );
       }
     } else {
+      if (props.setList) {
+        props.setList((prev: any) => [...prev, props.title] as never);
+      }
     }
   };
   return (
@@ -33,7 +28,6 @@ const SingleCheckBox = (props: Props) => {
       children={<span class="text-sm">{props.title}</span>}
       checked={instances().some((instance) => instance === props.title)}
       onChange={handleChange}
-      indeterminate={props.indeterminate}
     />
   );
 };
