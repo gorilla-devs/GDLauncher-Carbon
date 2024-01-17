b6b688eff927a7692b5b16b7b8061034500fe9d8
aymanesarrar <aymensarrar1@gmail.com>
2024-01-17 23:14:41 +0100

    fix linting

--- apps/desktop/packages/mainWindow/src/managers/ModalsManager/modals/OnBoarding/SingleCheckBox.tsx
+++ apps/desktop/packages/mainWindow/src/managers/ModalsManager/modals/OnBoarding/SingleCheckBox.tsx
@@ -1,5 +1,5 @@
 import { Checkbox } from "@gd/ui";
-import { Setter, createEffect } from "solid-js";
+import { Setter } from "solid-js";
 import { instances } from "./SingleEntity";
 
 interface Props {
