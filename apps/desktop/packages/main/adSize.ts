302d88a6649a50811a71471d053195f1b5468cbd
Blarfoon <davide.ceschia@gmail.com>
2024-01-23 20:29:57 +0100

    slightly increase min window height

--- apps/desktop/packages/main/adSize.ts
+++ apps/desktop/packages/main/adSize.ts
@@ -11,9 +11,9 @@ export default function getAdSize() {
     // Smaller ads (160/600)
     return {
       minWidth: 1024,
-      minHeight: 770,
+      minHeight: 790,
       width: 1024,
-      height: 770,
+      height: 790,
       adSize: {
         width: 160,
         height: 600
@@ -22,9 +22,9 @@ export default function getAdSize() {
   } else {
     return {
       minWidth: 1280,
-      minHeight: 770,
+      minHeight: 790,
       width: 1600,
-      height: 770,
+      height: 790,
       adSize: {
         width: 400,
         height: 600
