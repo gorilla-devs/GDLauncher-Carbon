e1a42cad1e4bec219af918d1aeee107b8a136355
aymanesarrar <aymensarrar1@gmail.com>
2024-01-23 21:34:10 +0100

    Export crash UI report

--- apps/desktop/packages/mainWindow/src/managers/ModalsManager/modals/InstanceExport/atoms/Exporting.tsx
+++ apps/desktop/packages/mainWindow/src/managers/ModalsManager/modals/InstanceExport/atoms/Exporting.tsx
@@ -5,28 +5,42 @@ import { rspc } from "@/utils/rspcClient";
 import { createEffect, createSignal } from "solid-js";
 import { setTaskId, taskId } from "@/utils/import";
 import { setExportStep } from "..";
+const [failedMsg, setFailedMsg] = createSignal<string | undefined>(undefined);
+export { failedMsg, setFailedMsg };
 export default function Exporting() {
   const [t] = useTransContext();
   const [progress, setProgress] = createSignal(0);
   const rspcContext = rspc.useContext();
+  const [failing, setFailing] = createSignal(false);
 
   createEffect(() => {
     async function runner() {
+      console.log("==============1================");
+      console.log("failed", failing());
       if (taskId() !== undefined) {
         const task = await rspcContext.client.query([
           "vtask.getTask",
           taskId() || null
         ]);
-
+        console.log("==============2================");
         if (task && task?.progress) {
           if (task.progress.type === "Known") {
             setProgress(Math.floor(task.progress.value * 100));
           }
+
+          if (task.progress.type === "Failed") {
+            setFailedMsg(task.progress.value.cause[1].display as string);
+            setExportStep(2);
+          }
         }
         const isFailed = task && task.progress;
+        console.log(isFailed, "isFailed");
         const isDownloaded = task === null;
-        if (isDownloaded || isFailed) {
+        if (isDownloaded) {
           setTaskId(undefined);
+        }
+        if (task?.progress.type === "Indeterminate") {
+          setFailing(!failing());
         }
         if (isDownloaded) {
           setExportStep(2);
