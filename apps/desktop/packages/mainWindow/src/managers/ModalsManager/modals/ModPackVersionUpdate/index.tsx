b1cddd4dc6f1994f37a686c566c6ba78ddb1e286
aymanesarrar <aymensarrar1@gmail.com>
2024-01-29 13:52:26 +0100

    add modal for modpack version update

---
+++ apps/desktop/packages/mainWindow/src/managers/ModalsManager/modals/ModPackVersionUpdate/index.tsx
@@ -0,0 +1,19 @@
+import { ModalProps } from "../..";
+import ModalLayout from "../../ModalLayout";
+
+const ModPackVersionUpdate = (props: ModalProps) => {
+  return (
+    <ModalLayout
+      noHeader={props.noHeader}
+      title={props?.title}
+      overflowHiddenDisabled={true}
+      noPadding={true}
+      scrollable="overflow-y-scroll scrollbar-hide"
+      // height="h-96"
+    >
+      <div class="flex flex-col p-4 w-120"></div>
+    </ModalLayout>
+  );
+};
+
+export default ModPackVersionUpdate;
