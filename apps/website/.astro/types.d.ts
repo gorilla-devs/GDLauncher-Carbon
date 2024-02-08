9b29ac671123a2140bfe83028add0ae2ef1def5b
aymanesarrar <aymensarrar1@gmail.com>
2024-02-08 10:06:42 +0100

    add gray color to the nav elements underline

--- apps/website/.astro/types.d.ts
+++ apps/website/.astro/types.d.ts
@@ -225,5 +225,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	type ContentConfig = typeof import("./..\src\content\config.js");
+	type ContentConfig = typeof import("../src/content/config.js");
 }
