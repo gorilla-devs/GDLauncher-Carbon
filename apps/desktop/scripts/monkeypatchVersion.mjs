2a9008e6dc7521c5548112a0ebb3bf66f7ec241b
Davide <davide.ceschia@gmail.com>
2024-09-09 02:03:30 +0200

    properly fix monkeypatch

--- apps/desktop/scripts/monkeypatchVersion.mjs
+++ apps/desktop/scripts/monkeypatchVersion.mjs
@@ -2,7 +2,7 @@ import path from "path";
 import fs from "fs";
 import { appVersion } from "@gd/config";
 
-const packageJsonPath = path.join(__dirname, "..", "package.json");
+const packageJsonPath = path.join("package.json");
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, "utf8"));
 
 const actualVersion = appVersion;
