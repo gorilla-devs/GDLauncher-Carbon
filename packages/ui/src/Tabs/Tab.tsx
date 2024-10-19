3ca77f0eecb3d1d6d4bd596b94c0a8cabc6e83cc
Davide <10165264+blarfoon@users.noreply.github.com>
2024-10-19 16:02:00 +0200

    Migrate some UI components to CVA & improve styles consistency (#379)
    * Improved styles for tabs, input and dropdown
    
    * refactor many components with cva & fix styles
    
    * more ui fixes
    
    * maybe fix
    
    * fix it
    
    * fixes

--- packages/ui/src/Tabs/Tab.tsx
+++ packages/ui/src/Tabs/Tab.tsx
@@ -10,8 +10,88 @@ import {
   splitProps,
 } from "solid-js";
 import { useTabsContext } from "./Tabs";
+import { cva, type VariantProps } from "class-variance-authority";
 
-interface Props extends JSX.HTMLAttributes<HTMLDivElement> {
+const tabStyles = cva(
+  "flex justify-center items-center text-center transition-colors",
+  {
+    variants: {
+      variant: {
+        block: "w-full",
+        traditional: "w-auto",
+        underline: "",
+      },
+      orientation: {
+        horizontal: "",
+        vertical: "",
+      },
+      isSelected: {
+        true: "text-lightSlate-50",
+        false: "text-lightSlate-800",
+      },
+      noPadding: {
+        true: "flex flex-col justify-center",
+        false: "",
+      },
+    },
+    compoundVariants: [
+      {
+        variant: "underline",
+        orientation: "horizontal",
+        class: "h-full",
+      },
+    ],
+    defaultVariants: {
+      variant: "block",
+      orientation: "horizontal",
+      isSelected: false,
+      noPadding: false,
+    },
+  }
+);
+
+const tabContentStyles = cva("font-500 capitalize flex items-center", {
+  variants: {
+    variant: {
+      underline: "",
+      block: "flex-1 h-full cursor-pointer rounded-xl",
+      traditional: "flex-1 h-full rounded-t-xl",
+    },
+    orientation: {
+      horizontal: "",
+      vertical: "",
+    },
+    isSelected: {
+      true: "",
+      false: "",
+    },
+    centerContent: {
+      true: "justify-center",
+      false: "",
+    },
+  },
+  compoundVariants: [
+    {
+      variant: "block",
+      isSelected: true,
+      class: "text-lightSlate-50 bg-darkSlate-800",
+    },
+    {
+      variant: "block",
+      isSelected: false,
+      class: "text-darkSlate-50",
+    },
+    {
+      variant: "traditional",
+      isSelected: true,
+      class: "bg-darkSlate-700",
+    },
+  ],
+});
+
+interface Props
+  extends JSX.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof tabStyles> {
   children: JSXElement | string | number;
   onClick?: () => void;
   ignored?: boolean;
@@ -21,7 +101,10 @@ interface Props extends JSX.HTMLAttributes<HTMLDivElement> {
 }
 
 const Tab = (_props: Props) => {
-  const [onclick, props] = splitProps(_props, ["onClick"]);
+  const [{ onClick, class: classProp }, props] = splitProps(_props, [
+    "onClick",
+    "class",
+  ]);
   const [index, setIndex] = createSignal(-1);
   let ref: HTMLDivElement;
 
@@ -63,23 +146,18 @@ const Tab = (_props: Props) => {
 
   return (
     <div
-      classList={{
-        "w-full": tabsContext?.variant() === "block",
-        "w-auto": tabsContext?.variant() === "traditional",
-        "h-full":
-          tabsContext?.variant() === "underline" &&
-          tabsContext?.orientation() === "horizontal",
-        "cursor-pointer": !props.noPointer,
-        "flex flex-col justify-center": props.noPadding,
-        "text-lightSlate-50": tabsContext?.isSelectedIndex(index()),
-        "text-lightSlate-800": !tabsContext?.isSelectedIndex(index()),
-      }}
-      class="bg-darkSlate-800 hover:text-lightSlate-50 flex items-center"
+      class={tabStyles({
+        variant: tabsContext?.variant(),
+        orientation: tabsContext?.orientation(),
+        isSelected: tabsContext?.isSelectedIndex(index()),
+        noPadding: props.noPadding,
+        class: `bg-darkSlate-800 hover:text-lightSlate-50 ${classProp}`,
+      })}
       ref={(el) => {
         ref = el;
       }}
       onClick={() => {
-        if (onclick.onClick) onclick.onClick();
+        if (onClick) onClick();
         if (!props.ignored) tabsContext?.setSelectedIndex(index());
       }}
       {...props}
@@ -87,61 +165,41 @@ const Tab = (_props: Props) => {
       <Switch>
         <Match when={tabsContext?.variant() === "underline"}>
           <div
-            class={`hover:text-lightSlate-50 transition-colors font-500 capitalize flex items-center h-full ${
-              tabsContext?.paddingX?.() || ""
-            } ${tabsContext?.paddingY?.() || ""}`}
-            classList={{
-              "border-box": tabsContext?.orientation() === "horizontal",
-              "py-2":
-                tabsContext?.orientation() === "vertical" &&
-                !tabsContext?.paddingY?.() &&
-                !props.noPadding,
-              "px-5":
-                tabsContext?.orientation() === "vertical" &&
-                !tabsContext?.paddingX?.(),
-              "justify-center": props.centerContent,
-            }}
+            class={tabContentStyles({
+              variant: "underline",
+              orientation: tabsContext?.orientation(),
+              centerContent: props.centerContent,
+              class: `${tabsContext?.paddingX?.() || ""} ${
+                tabsContext?.paddingY?.() || ""
+              }`,
+            })}
           >
             {props.children}
           </div>
         </Match>
         <Match when={tabsContext?.variant() === "block"}>
           <div
-            class={`flex gap-1 hover:text-lightSlate-50 justify-center items-center flex-1 h-full cursor-pointer rounded-xl font-500 capitalize box-border ${
-              tabsContext?.paddingX?.() || ""
-            } ${tabsContext?.paddingY?.() || ""}`}
-            classList={{
-              "py-5":
-                tabsContext?.orientation() === "horizontal" &&
-                !tabsContext?.paddingY?.(),
-              "px-2":
-                tabsContext?.orientation() === "horizontal" &&
-                !tabsContext?.paddingX?.(),
-              "px-4":
-                tabsContext?.orientation() === "vertical" &&
-                !tabsContext?.paddingX?.(),
-              "py-2":
-                tabsContext?.orientation() === "vertical" &&
-                !tabsContext?.paddingY?.(),
-              "text-lightSlate-50 bg-darkSlate-800":
-                tabsContext?.isSelectedIndex(index()),
-              "text-darkSlate-50": !tabsContext?.isSelectedIndex(index()),
-            }}
+            class={tabContentStyles({
+              variant: "block",
+              orientation: tabsContext?.orientation(),
+              isSelected: tabsContext?.isSelectedIndex(index()),
+              class: `${tabsContext?.paddingX?.() || ""} ${
+                tabsContext?.paddingY?.() || ""
+              }`,
+            })}
           >
             {props.children}
           </div>
         </Match>
         <Match when={tabsContext?.variant() === "traditional"}>
           <div
-            class={`flex gap-1 hover:text-lightSlate-50 justify-center items-center bg-darkSlate-800 flex-1 h-full font-500 capitalize box-border rounded-t-xl ${
-              tabsContext?.paddingX?.() || ""
-            } ${tabsContext?.paddingY?.() || ""}`}
-            classList={{
-              "px-2": !tabsContext?.paddingX?.(),
-              "text-lightSlate-50": tabsContext?.isSelectedIndex(index()),
-              "text-lightSlate-800": !tabsContext?.isSelectedIndex(index()),
-              "bg-darkSlate-700": tabsContext?.isSelectedIndex(index()),
-            }}
+            class={tabContentStyles({
+              variant: "traditional",
+              isSelected: tabsContext?.isSelectedIndex(index()),
+              class: `${tabsContext?.paddingX?.() || ""} ${
+                tabsContext?.paddingY?.() || ""
+              }`,
+            })}
           >
             {props.children}
           </div>
