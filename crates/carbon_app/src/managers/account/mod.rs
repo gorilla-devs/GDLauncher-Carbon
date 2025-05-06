d02133214a775317a03fdd3dd6ce238aaa687eff
Davide <davide.ceschia@gmail.com>
2025-05-06 02:58:42 +0200

    remove unnecessary log

--- crates/carbon_app/src/managers/account/mod.rs
+++ crates/carbon_app/src/managers/account/mod.rs
@@ -1,18 +1,18 @@
 use crate::api::keys::settings::GET_SETTINGS;
-use crate::api::{update_core_module_status, CoreModuleStatus};
+use crate::api::{CoreModuleStatus, update_core_module_status};
 use crate::domain::account::*;
 use crate::{
     api::keys::account::*,
     managers::account::{api::GetProfileError, enroll::InvalidateCtx},
 };
+use anyhow::{Context, ensure};
 use anyhow::{anyhow, bail};
-use anyhow::{ensure, Context};
 use async_trait::async_trait;
 use axum::extract;
 use carbon_repos::db::app_configuration;
 use carbon_repos::db::{self, read_filters::StringFilter};
 use carbon_repos::pcr::{
-    chrono::DateTime, prisma_errors::query_engine::RecordNotFound, Direction, QueryError,
+    Direction, QueryError, chrono::DateTime, prisma_errors::query_engine::RecordNotFound,
 };
 use chrono::{FixedOffset, Utc};
 use gdl_account::{
@@ -22,7 +22,7 @@ use gdl_account::{
 use jwt::{Header, Token};
 use reqwest::Client;
 use reqwest_middleware::ClientBuilder;
-use reqwest_retry::{policies::ExponentialBackoff, RetryTransientMiddleware};
+use reqwest_retry::{RetryTransientMiddleware, policies::ExponentialBackoff};
 use serde::{Deserialize, Serialize};
 use std::cmp::Ordering;
 use std::{
@@ -210,7 +210,9 @@ impl<'s> ManagerRef<'s, AccountManager> {
             ))?
             .id_token
         else {
-            bail!("this account is present in the db but the id_token is missing. Presumably offline account. (uuid: {uuid})");
+            bail!(
+                "this account is present in the db but the id_token is missing. Presumably offline account. (uuid: {uuid})"
+            );
         };
 
         info!("Waiting for account validation");
@@ -231,7 +233,9 @@ impl<'s> ManagerRef<'s, AccountManager> {
             ))?
             .id_token
         else {
-            bail!("this account is present in the db but the id_token is missing. Presumably offline account. (uuid: {uuid})");
+            bail!(
+                "this account is present in the db but the id_token is missing. Presumably offline account. (uuid: {uuid})"
+            );
         };
 
         Ok(self.gdl_account_task.get_account(id_token).await?)
@@ -255,7 +259,9 @@ impl<'s> ManagerRef<'s, AccountManager> {
             .id_token
         else {
             return Err(RequestGDLAccountDeletionError::RequestFailed(
-                anyhow::anyhow!("this account is present in the db but the id_token is missing. Presumably offline account. (uuid: {uuid})")
+                anyhow::anyhow!(
+                    "this account is present in the db but the id_token is missing. Presumably offline account. (uuid: {uuid})"
+                ),
             ));
         };
 
@@ -290,7 +296,9 @@ impl<'s> ManagerRef<'s, AccountManager> {
             ))?
             .id_token
         else {
-            bail!("this account is present in the db but the id_token is missing. Presumably offline account. (uuid: {uuid})");
+            bail!(
+                "this account is present in the db but the id_token is missing. Presumably offline account. (uuid: {uuid})"
+            );
         };
 
         let user = self
@@ -349,7 +357,9 @@ impl<'s> ManagerRef<'s, AccountManager> {
             ))?
             .id_token
         else {
-            bail!("this account is present in the db but the id_token is missing. Presumably offline account. (uuid: {saved_gdl_account_uuid})")
+            bail!(
+                "this account is present in the db but the id_token is missing. Presumably offline account. (uuid: {saved_gdl_account_uuid})"
+            )
         };
 
         let Some(user) = self.gdl_account_task.get_account(id_token).await? else {
@@ -375,7 +385,9 @@ impl<'s> ManagerRef<'s, AccountManager> {
             .id_token
         else {
             return Err(RequestNewVerificationTokenError::RequestFailed(
-                anyhow::anyhow!("this account is present in the db but the id_token is missing. Presumably offline account. (uuid: {uuid})")
+                anyhow::anyhow!(
+                    "this account is present in the db but the id_token is missing. Presumably offline account. (uuid: {uuid})"
+                ),
             ));
         };
 
@@ -1085,12 +1097,17 @@ impl AccountRefreshService {
                     });
 
                     for account in accounts {
-                        trace!("Considering checking account: {account:#?}");
+                        trace!(
+                            "Considering checking account: {} {}",
+                            account.uuid, account.username
+                        );
 
                         let uuid = account.uuid.clone();
                         // ignore badly formed account entries since we can't handle them
                         let Ok(account) = FullAccount::try_from(account) else {
-                            tracing::error!("Badly formed account entry for uuid {uuid}. Cannot check refresh status.");
+                            tracing::error!(
+                                "Badly formed account entry for uuid {uuid}. Cannot check refresh status."
+                            );
                             continue;
                         };
                         let token_expires = match account.type_ {
@@ -1166,9 +1183,7 @@ pub enum GetActiveAccountError {
 
 #[derive(Error, Debug)]
 pub enum GetAccountStatusError {
-    #[error(
-        "getting account status: microsoft account token expiry date is unset (invalid state)"
-    )]
+    #[error("getting account status: microsoft account token expiry date is unset (invalid state)")]
     TokenExpiryUnset,
 
     #[error("getting account status: microsoft account token is unset")]
@@ -1379,6 +1394,8 @@ impl From<api::FullAccount> for FullAccount {
 
 #[derive(Error, Debug)]
 pub enum FullAccountLoadError {
-    #[error("attempted to parse microsoft account DB entry(uuid {0}), but was missing refresh token expiration timestamp")]
+    #[error(
+        "attempted to parse microsoft account DB entry(uuid {0}), but was missing refresh token expiration timestamp"
+    )]
     MissingExpiration(String),
 }
