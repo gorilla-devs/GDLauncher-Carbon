46388f8297cd906143a45fb06fcebda040210d23
kazaminosuke <99616465+kazaminosuke@users.noreply.github.com>
2025-11-29 10:58:24 +0900

    Update index.ts

--- packages/i18n/index.ts
+++ packages/i18n/index.ts
@@ -11,7 +11,7 @@ import languagesData from "./languages.json" with { type: "json" }
 
 export const supportedLanguages = {
   english: "united-states",
-  italian: "italy"
+  italian: "italy",
   japanese: "japan"
 }
 
