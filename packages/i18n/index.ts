1f19b4405a570b000a479befe03f26b27a3bdb73
Iewnfod <iewnfoddd@outlook.com>
2026-07-13 18:37:03 +0800

    Add simplified chinese translation

--- packages/i18n/index.ts
+++ packages/i18n/index.ts
@@ -12,7 +12,8 @@ import languagesData from "./languages.json" with { type: "json" }
 export const supportedLanguages = {
   english: "united-states",
   italian: "italy",
-  japanese: "japan"
+  japanese: "japan",
+  simplified_chinese: "china",
 }
 
 type NamespacesMap = {
