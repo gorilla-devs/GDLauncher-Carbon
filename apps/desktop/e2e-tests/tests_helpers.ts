c4417563e0dc4d3cac19696153dc06428fa7f4f8
Davide Ceschia <davide.ceschia@gmail.com>
2026-07-25 22:00:24 +0200

    Removes the dead getActualUrl helper
    Both specs use getActualPath; nothing imports getActualUrl.

--- apps/desktop/e2e-tests/tests_helpers.ts
+++ apps/desktop/e2e-tests/tests_helpers.ts
@@ -1,9 +1,3 @@
-export const getActualUrl = (url: string) => {
-  const split = url.split("index.html#")
-  const { pathname, search } = new URL(`http://bruh.gdlauncher.com${split[1]}`)
-  return `${pathname}${search}`
-}
-
 /** The route alone, ignoring query state such as queued modals. */
 export const getActualPath = (url: string) => {
   const split = url.split("index.html#")
